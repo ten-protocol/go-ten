f6351c37eacee272e6bcea101e059e45b60ac84f
tudor.malene@gmail.com <tudor.malene@gmail.com>
2022-03-24 09:40:57 +0000

    address pr comment

--- go/obscuronode/enclave/rollup.go
+++ go/obscuronode/enclave/rollup.go
@@ -58,8 +58,7 @@ func NewRollup(b *types.Block, parent *Rollup, a common.Address, txs []nodecommo
 func (r *Rollup) ProofHeight(l1BlockResolver BlockResolver) uint64 {
 	v, f := l1BlockResolver.FetchBlock(r.Header.L1Proof)
 	if !f {
-		// return GenesisHeight-1
-		return 0
+		return obscurocommon.L1GenesisHeight - 1
 	}
 	return l1BlockResolver.HeightBlock(v)
 }
