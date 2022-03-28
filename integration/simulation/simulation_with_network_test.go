f30a3eda6d811b897f5040a0cc68147a1e097697
joeldudley <>
2022-03-28 11:39:26 +0100

    Checkpoint.

--- integration/simulation/simulation_with_network_test.go
+++ integration/simulation/simulation_with_network_test.go
@@ -1,10 +1,60 @@
 package simulation
 
 import (
+	"github.com/google/uuid"
+	"math/rand"
 	"testing"
+	"time"
 )
 
-// Todo - Joel
 // This test creates a network of standalone L2 nodes, then injects transactions, and finally checks the resulting output blockchain
 func TestStandaloneNodesMonteCarloSimulation(t *testing.T) {
+	// todo - joel - pull some of this setup into a common method
+	// define core test parameters
+	numberOfNodes := 10
+	numberOfWallets := 5
+
+	simulationTimeSecs := 15 // in seconds
+
+	// This is a critical parameter of the simulation. The value should be as low as possible, as long as the test is still meaningful
+	avgBlockDurationUSecs := uint64(100_000) // in u seconds 1 sec = 1e6 usecs.
+
+	avgNetworkLatency := avgBlockDurationUSecs / 15 // artificial latency injected between sending and receiving messages on the mock network
+	avgGossipPeriod := avgBlockDurationUSecs / 3    // POBI protocol setting
+
+	// converted to Us
+	simulationTimeUSecs := simulationTimeSecs * 1000 * 1000
+
+	rand.Seed(time.Now().UnixNano())
+	uuid.EnableRandPool()
+
+	logFile := setupTestLog("../.build/simulations/")
+	defer logFile.Close()
+
+	stats := NewStats(numberOfNodes)
+
+	mockEthNodes, obscuroInMemNodes := CreateBasicNetworkOfStandaloneNodes(numberOfNodes, avgGossipPeriod, avgBlockDurationUSecs, avgNetworkLatency, stats)
+
+	txInjector := NewTransactionInjector(numberOfWallets, avgBlockDurationUSecs, stats, simulationTimeUSecs, mockEthNodes, obscuroInMemNodes)
+
+	// todo - joel - change `InMemObscuroNodes` to just `ObscuroNodes`
+	simulation := Simulation{
+		MockEthNodes:       mockEthNodes,      // the list of mock ethereum nodes
+		InMemObscuroNodes:  obscuroInMemNodes, // the list of in memory obscuro nodes
+		AvgBlockDuration:   avgBlockDurationUSecs,
+		TxInjector:         txInjector,
+		SimulationTimeSecs: simulationTimeSecs,
+		Stats:              stats,
+	}
+
+	// execute the simulation
+	simulation.Start()
+
+	// run tests
+	checkBlockchainValidity(t, &simulation)
+
+	// generate and print the final stats
+	t.Logf("Simulation results:%+v", NewOutputStats(&simulation))
+
+	simulation.Stop()
 }
--- integration/simulation/utils.go
+++ integration/simulation/utils.go
@@ -2,6 +2,7 @@ package simulation
 
 import (
 	"fmt"
+	"github.com/obscuronet/obscuro-playground/go/obscuronode/host/p2p"
 	"math/big"
 	"os"
 	"time"
@@ -14,6 +15,11 @@ import (
 
 	"github.com/obscuronet/obscuro-playground/go/obscurocommon"
 	ethereum_mock "github.com/obscuronet/obscuro-playground/integration/ethereummock"
+)
+
+const (
+	localhost    = "localhost"
+	p2pStartPort = 10000
 )
 
 func setupTestLog(baseDir string) *os.File {
@@ -53,6 +59,20 @@ func createInMemObscuroNode(id int64, genesis bool, avgGossipPeriod uint64, avgB
 	return &node
 }
 
+func createStandaloneObscuroNode(id int64, genesis bool, avgGossipPeriod uint64, stats *Stats, nodeAddr string, peerAddrs []string) *host.Node {
+	nodeP2p := p2p.NewSocketObscuroNetwork(nodeAddr, peerAddrs)
+
+	obscuroNodeCfg := defaultObscuroNodeCfg(avgGossipPeriod)
+
+	nodeID := common.BigToAddress(big.NewInt(id))
+	// todo - joel - switch to standalone enclave
+	enclaveClient := enclave.NewEnclave(nodeID, true, stats)
+
+	// create a standalone memory obscuro node
+	node := host.NewObscuroAggregator(nodeID, obscuroNodeCfg, nil, stats, genesis, enclaveClient, nodeP2p)
+	return &node
+}
+
 // creates the nodes, wires them up, and populates the network objects
 func CreateBasicNetworkOfInMemoryNodes(nrNodes int, avgGossipPeriod uint64, avgBlockDurationUSecs uint64, avgLatency uint64, stats *Stats) ([]*ethereum_mock.Node, []*host.Node) {
 	// todo - add observer nodes
@@ -80,6 +100,45 @@ func CreateBasicNetworkOfInMemoryNodes(nrNodes int, avgGossipPeriod uint64, avgB
 	for i := 0; i < nrNodes; i++ {
 		l1Nodes[i].Network.(*ethereum_mock.MockEthNetwork).AllNodes = l1Nodes
 		l2Nodes[i].P2p.(*ObscuroInMemNetwork).Nodes = l2Nodes
+	}
+
+	return l1Nodes, l2Nodes
+}
+
+// creates the nodes, wires them up, and populates the network objects
+func CreateBasicNetworkOfStandaloneNodes(nrNodes int, avgGossipPeriod uint64, avgBlockDurationUSecs uint64, avgLatency uint64, stats *Stats) ([]*ethereum_mock.Node, []*host.Node) {
+	// todo - add observer nodes
+	// todo - joel - connect to real L1 nodes
+	l1Nodes := make([]*ethereum_mock.Node, nrNodes)
+	l2Nodes := make([]*host.Node, nrNodes)
+
+	var nodeAddrs []string
+	for i := 0; i < nrNodes; i++ {
+		// We assign a P2P address to each node on the network.
+		nodeAddrs = append(nodeAddrs, fmt.Sprintf("%s:%d", localhost, p2pStartPort+i))
+	}
+
+	for i := 1; i <= nrNodes; i++ {
+		genesis := false
+		if i == 1 {
+			genesis = true
+		}
+
+		// create the in memory l1 and l2 node
+		miner := createMockEthNode(int64(i), nrNodes, avgBlockDurationUSecs, avgLatency, stats)
+		agg := createStandaloneObscuroNode(int64(i), genesis, avgGossipPeriod, stats, nodeAddrs[i-1], nodeAddrs)
+
+		// and connect them to each other
+		agg.ConnectToEthNode(miner)
+		miner.AddClient(agg)
+
+		l1Nodes[i-1] = miner
+		l2Nodes[i-1] = agg
+	}
+
+	// populate the nodes field of the L1 network
+	for i := 0; i < nrNodes; i++ {
+		l1Nodes[i].Network.(*ethereum_mock.MockEthNetwork).AllNodes = l1Nodes
 	}
 
 	return l1Nodes, l2Nodes
