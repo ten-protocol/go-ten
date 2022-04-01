f72081827fee893e82d5ec2e8d4200398f90cb15
tudor.malene@gmail.com <tudor.malene@gmail.com>
2022-04-01 15:28:38 +0100

    efficiency fix

--- integration/simulation/simulation_in_mem_test.go
+++ integration/simulation/simulation_in_mem_test.go
@@ -13,14 +13,14 @@ func TestInMemoryMonteCarloSimulation(t *testing.T) {
 	params := SimParams{
 		NumberOfNodes:         10,
 		NumberOfWallets:       5,
-		AvgBlockDurationUSecs: uint64(25_000),
+		AvgBlockDurationUSecs: uint64(30_000),
 		SimulationTimeSecs:    15,
 	}
 	params.AvgNetworkLatency = params.AvgBlockDurationUSecs / 15
 	params.AvgGossipPeriod = params.AvgBlockDurationUSecs / 3
 	params.SimulationTimeUSecs = params.SimulationTimeSecs * 1000 * 1000
 
-	efficiencies := EfficiencyThresholds{0.2, 0.3, 0.32}
+	efficiencies := EfficiencyThresholds{0.2, 0.32, 0.34}
 
 	testSimulation(t, CreateBasicNetworkOfInMemoryNodes, params, efficiencies)
 }
