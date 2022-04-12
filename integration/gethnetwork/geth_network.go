978b9fdcfd6bb049bef296a314554bec4ba259e4
joel dudley <joel@obscu.ro>
2022-04-12 08:23:36 +0100

    Creates genesis.json file dynamically.

--- integration/gethnetwork/geth_network.go
+++ integration/gethnetwork/geth_network.go
@@ -9,12 +9,12 @@ import (
 	"time"
 )
 
-// TODO - Create genesis file from string on-the-fly, rather than pointing to a real location.
-
 const (
 	genesisFileName = "genesis.json"
+	genesisConfig   = "{\n  \"config\": {\n    \"chainId\": 777,\n    \"homesteadBlock\": 0,\n    \"eip150Block\": 0,\n    \"eip155Block\": 0,\n    \"eip158Block\": 0,\n    \"byzantiumBlock\": 0,\n    \"constantinopleBlock\": 0,\n    \"petersburgBlock\": 0,\n    \"istanbulBlock\": 0,\n    \"berlinBlock\": 0,\n    \"londonBlock\": 0\n  },\n  \"alloc\": {\n    \"0x0000000000000000000000000000000000000001\": {\n      \"balance\": \"111111111\"\n    }\n  },\n  \"coinbase\": \"0x0000000000000000000000000000000000000000\",\n  \"difficulty\": \"0x20000\",\n  \"extraData\": \"\",\n  \"gasLimit\": \"0x2fefd8\",\n  \"nonce\": \"0x0000000000000042\",\n  \"mixhash\": \"0x0000000000000000000000000000000000000000000000000000000000000000\",\n  \"parentHash\": \"0x0000000000000000000000000000000000000000000000000000000000000000\",\n  \"timestamp\": \"0x00\"\n}"
 	nodeFolderName  = string(os.PathSeparator) + "node%d_datadir" + string(os.PathSeparator)
 	ipcFileName     = "geth.ipc"
+	tempDirPrefix   = "geth_nodes"
 
 	dataDirFlag = "--datadir"
 	execFlag    = "--exec"
@@ -28,13 +28,21 @@ const (
 
 // GethNetwork is a network of Geth nodes, built using the provided Geth binary.
 type GethNetwork struct {
-	gethBinaryPath string
-	dataDirs       []string
+	gethBinaryPath  string
+	genesisFilePath string
+	dataDirs        []string
 }
 
 // NewGethNetwork using the provided Geth binary to create a private Ethereum network with numNodes Geth nodes.
 func NewGethNetwork(gethBinaryPath string, numNodes int) GethNetwork {
-	nodesDir, err := ioutil.TempDir("", "geth_nodes")
+	nodesDir, err := ioutil.TempDir("", tempDirPrefix)
+	if err != nil {
+		panic(err)
+	}
+
+	// We write out the `genesis.json` file to be used by the network.
+	genesisFilePath := nodesDir + string(os.PathSeparator) + genesisFileName
+	err = os.WriteFile(genesisFilePath, []byte(genesisConfig), 0o700)
 	if err != nil {
 		panic(err)
 	}
@@ -46,8 +54,9 @@ func NewGethNetwork(gethBinaryPath string, numNodes int) GethNetwork {
 	}
 
 	network := GethNetwork{
-		gethBinaryPath: gethBinaryPath,
-		dataDirs:       dataDirs,
+		gethBinaryPath:  gethBinaryPath,
+		genesisFilePath: genesisFilePath,
+		dataDirs:        dataDirs,
 	}
 
 	for i, dataDir := range dataDirs {
@@ -84,15 +93,10 @@ func (network *GethNetwork) createGethNode(dataDir string, port int) {
 
 // Initialises a Geth node based on the network genesis file.
 func (network *GethNetwork) initGethNode(dataDirPath string) {
-	workingDir, err := os.Getwd()
-	if err != nil {
-		panic(err)
-	}
-
-	args := []string{dataDirFlag, dataDirPath, initCmd, workingDir + string(os.PathSeparator) + genesisFileName}
+	args := []string{dataDirFlag, dataDirPath, initCmd, network.genesisFilePath}
 	cmd := exec.Command(network.gethBinaryPath, args...) // nolint
 
-	err = cmd.Run()
+	err := cmd.Run()
 	if err != nil {
 		panic(err)
 	}
