4a483df1d5d0fe98c039c553f9416a9abd564aa4
Pedro Gomes <otherview@gmail.com>
2022-04-18 20:30:45 +0100

    adding the contract

---
+++ contracts/rollup.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract RollupStorage {
+
+    mapping(uint256 => string[]) public rollups;
+    mapping(address => uint256) public deposits;
+    string secret;
+
+    function AddRollup(string memory rollupData) public {
+        rollups[block.number].push(rollupData);
+    }
+
+
+    function Rollup() public view returns (string[] memory){
+        return rollups[block.number];
+    }
+
+    function Deposit(address dest) public payable {
+        deposits[dest] = msg.value;
+    }
+
+    function StoreSecret(string memory inputSecret) public {
+        secret = inputSecret;
+    }
+
+    function RequestSecret() public view  returns (string memory) {
+        return secret;
+    }
+
+    function Withdraw(uint256 withdrawAmount, address payable destination) public {
+        destination.transfer(withdrawAmount);
+    }
+
+}
