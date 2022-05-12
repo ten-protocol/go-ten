db1218307c5787ddd51c5851be5c098fba4e7095
joeldudley <joel@obscu.ro>
2022-05-12 11:36:41 +0100

    Address todo.

--- go/obscuronode/host/in_mem_obscuro_client.go
+++ go/obscuronode/host/in_mem_obscuro_client.go
@@ -1,6 +1,8 @@
 package host
 
 import (
+	"fmt"
+
 	"github.com/obscuronet/obscuro-playground/go/obscuronode/nodecommon"
 	"github.com/obscuronet/obscuro-playground/go/obscuronode/obscuroclient"
 )
@@ -19,8 +21,16 @@ func NewInMemObscuroClient(p2p P2P) obscuroclient.Client {
 // Call bypasses RPC, and invokes methods on the node directly.
 func (c inMemObscuroClient) Call(_ interface{}, method string, args ...interface{}) error {
 	if method == obscuroclient.RPCSendTransactionEncrypted {
-		// todo - joel - be more resilient to errors here
-		c.obscuroAPI.SendTransactionEncrypted(args[0].(nodecommon.EncryptedTx))
+		// TODO - Extract this checking logic as the set of RPC operations grows.
+		if len(args) != 0 {
+			return fmt.Errorf("expected 0 args to %s, got %d", obscuroclient.RPCSendTransactionEncrypted, len(args))
+		}
+		tx, ok := args[0].(nodecommon.EncryptedTx)
+		if !ok {
+			return fmt.Errorf("arg to %s was not of expected type EncryptedTx", obscuroclient.RPCSendTransactionEncrypted)
+		}
+
+		c.obscuroAPI.SendTransactionEncrypted(tx)
 	}
 	return nil
 }
