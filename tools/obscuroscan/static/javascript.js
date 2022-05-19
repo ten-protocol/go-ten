2861e3fa79700a406bdfbbd2c664a890ea39ad96
joeldudley <joel@obscu.ro>
2022-05-19 10:14:03 +0100

    Renames file. Makes Obscuroscan update chain height on a timer.

--- tools/obscuroscan/static/javascript.js
+++ tools/obscuroscan/static/javascript.js
@@ -1,22 +1,19 @@
-const eventClick = "click";
 const eventDomLoaded = "DOMContentLoaded";
-const idGetBlockHeadHeight = "getBlockHeadHeight";
 const idBlockHeadHeight = "blockHeadHeight";
 const pathBlockHeadHeight = "/blockheadheight/";
 
 const initialize = () => {
-    const getBlockHeadHeightButton = document.getElementById(idGetBlockHeadHeight);
     const statusArea = document.getElementById(idBlockHeadHeight);
 
-    getBlockHeadHeightButton.addEventListener(eventClick, async () => {
+    setInterval(async () => {
         const blockHeadHeightResp = await fetch(pathBlockHeadHeight);
 
         if (blockHeadHeightResp.ok) {
-            statusArea.innerText = `Current block head height: ${await blockHeadHeightResp.text()}`;
+            statusArea.innerText = await blockHeadHeightResp.text();
         } else {
             statusArea.innerText = "Failed to retrieve block head height."
         }
-    })
+    }, 2000);
 }
 
 window.addEventListener(eventDomLoaded, initialize);
