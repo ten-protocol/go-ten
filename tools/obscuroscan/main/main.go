dbbedd4be4e48558b47590de4728f9df5f4b5a8a
joeldudley <joel@obscu.ro>
2022-05-20 11:34:18 +0100

    Self-review.

--- tools/obscuroscan/main/main.go
+++ tools/obscuroscan/main/main.go
@@ -2,22 +2,18 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/obscuronet/obscuro-playground/tools/obscuroscan"
 )
 
-const localhost = "0.0.0.0:"
-
 func main() {
 	config := parseCLIArgs()
 	nodeID := common.BytesToAddress([]byte(config.nodeID))
-	obscuroscanAddr := localhost + strconv.Itoa(config.startPort)
 
 	server := obscuroscan.NewObscuroscan(nodeID, config.clientServerAddr)
-	go server.Serve(obscuroscanAddr)
-	fmt.Printf("Obscuroscan started.\n💡 Visit %s to monitor the Obscuro network.\n", obscuroscanAddr)
+	go server.Serve(config.address)
+	fmt.Printf("Obscuroscan started.\n💡 Visit %s to monitor the Obscuro network.\n", config.address)
 
 	defer server.Shutdown()
 	select {}
