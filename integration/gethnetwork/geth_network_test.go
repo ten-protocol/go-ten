07c661020d804bdf01ce78ee4821681a0d613bd5
joeldudley <joel@obscu.ro>
2022-05-24 22:18:39 +0100

    Removes todo.

--- integration/gethnetwork/geth_network_test.go
+++ integration/gethnetwork/geth_network_test.go
@@ -96,7 +96,6 @@ func TestGethTransactionCanBeSubmitted(t *testing.T) {
 	// check the transaction has expected values
 	issuedTx := map[string]interface{}{}
 	if err = yaml.Unmarshal([]byte(issuedTxStr), issuedTx); err != nil {
-		// todo - joel - update this to show previous msg as well
 		t.Fatalf("unable to unmarshall getTransaction response to YAML. Cause: %s.\nsendTransaction response was: %s\ngetTransaction response was %s", err, txHash, issuedTxStr)
 	}
 
