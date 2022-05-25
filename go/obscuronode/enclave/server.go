0b0ba8eb2043eba3331314a807b0d9af31b91d95
joeldudley <joel@obscu.ro>
2022-05-25 21:52:55 +0100

    Logging to help with debugging.

--- go/obscuronode/enclave/server.go
+++ go/obscuronode/enclave/server.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"github.com/obscuronet/obscuro-playground/go/log"
 	"net"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -70,6 +71,7 @@ func StartServer(
 
 // IsReady returns a nil error to indicate that the server is ready.
 func (s *server) IsReady(context.Context, *generated.IsReadyRequest) (*generated.IsReadyResponse, error) {
+	log.Info("Received isReady request.")
 	errStr := ""
 	if err := s.enclave.IsReady(); err != nil {
 		errStr = err.Error()
