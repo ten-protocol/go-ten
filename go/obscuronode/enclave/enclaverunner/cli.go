76701b5129490936c76a0335dcffca9d09968753
joeldudley <joel@obscu.ro>
2022-05-26 11:47:52 +0100

    Can parse node config from .toml.

--- go/obscuronode/enclave/enclaverunner/cli.go
+++ go/obscuronode/enclave/enclaverunner/cli.go
@@ -9,39 +9,6 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 )
 
-const (
-	// Flag names, defaults and usages.
-	HostIDName  = "hostID"
-	hostIDUsage = "The 20 bytes of the address of the Obscuro host this enclave serves"
-
-	AddressName  = "address"
-	addressUsage = "The address on which to serve the Obscuro enclave service"
-
-	chainIDName  = "chainID"
-	chainIDUsage = "A integer representing the unique chain id the enclave will connect to (default 1337)"
-
-	willAttestName  = "willAttest"
-	willAttestUsage = "Whether the enclave will produce a verified attestation report"
-
-	validateL1BlocksName  = "validateL1Blocks"
-	validateL1BlocksUsage = "Whether to validate incoming blocks using the hardcoded L1 genesis.json config"
-
-	speculativeExecutionName  = "speculativeExecution"
-	speculativeExecutionUsage = "Whether to enable speculative execution"
-
-	managementContractAddressName  = "managementContractAddress"
-	managementContractAddressUsage = "The management contract address on the L1"
-
-	erc20contractAddrsName  = "erc20ContractAddresses"
-	erc20contractAddrsUsage = "The ERC20 contract addresses to monitor on the L1"
-
-	writeToLogsName  = "writeToLogs"
-	writeToLogsUsage = "Whether to redirect the output to the log file."
-
-	logPathName  = "logPath"
-	logPathUsage = "The path to use for the enclave service's log file"
-)
-
 func ParseCLIArgs() config.EnclaveConfig {
 	defaultConfig := config.DefaultEnclaveConfig()
 
