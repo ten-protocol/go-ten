31c4ffe18362da3e6b664f1d1d8ec638d7e09d90
Tudor Malene <tudor.malene@gmail.com>
2022-05-30 11:13:23 +0100

    Reinstate tests

--- integration/simulation/network/geth_network.go
+++ integration/simulation/network/geth_network.go
@@ -11,8 +11,138 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/obscuronet/obscuro-playground/go/ethclient"
+	"github.com/obscuronet/obscuro-playground/go/ethclient/erc20contractlib"
+	"github.com/obscuronet/obscuro-playground/go/ethclient/mgmtcontractlib"
 	"github.com/obscuronet/obscuro-playground/go/log"
+	"github.com/obscuronet/obscuro-playground/go/obscuronode/host"
+	"github.com/obscuronet/obscuro-playground/go/obscuronode/obscuroclient"
+	"github.com/obscuronet/obscuro-playground/integration/erc20contract"
+	"github.com/obscuronet/obscuro-playground/integration/gethnetwork"
+	"github.com/obscuronet/obscuro-playground/integration/simulation/p2p"
+	"github.com/obscuronet/obscuro-playground/integration/simulation/params"
+	"github.com/obscuronet/obscuro-playground/integration/simulation/stats"
 )
+
+type networkInMemGeth struct {
+	obscuroClients []*obscuroclient.Client
+	gethNetwork    *gethnetwork.GethNetwork
+	wallets        []wallet.Wallet
+	contracts      []string
+	workerWallet   wallet.Wallet
+}
+
+func NewNetworkInMemoryGeth(wallets []wallet.Wallet, workerWallet wallet.Wallet, contracts []string) Network {
+	return &networkInMemGeth{
+		wallets:      wallets,
+		contracts:    contracts,
+		workerWallet: workerWallet,
+	}
+}
+
+// Create inits and starts the nodes, wires them up, and populates the network objects
+func (n *networkInMemGeth) Create(params *params.SimParams, stats *stats.Stats) ([]ethclient.EthClient, []*obscuroclient.Client, []string, error) {
+	// make sure the geth network binaries exist
+	path, err := gethnetwork.EnsureBinariesExist(gethnetwork.LatestVersion)
+	if err != nil {
+		panic(err)
+	}
+
+	// get wallet addresses to prefund them
+	walletAddresses := make([]string, len(n.wallets))
+	for i, w := range n.wallets {
+		walletAddresses[i] = w.Address().String()
+	}
+
+	// kickoff the network with the prefunded wallet addresses
+	n.gethNetwork = gethnetwork.NewGethNetwork(
+		params.StartPort,
+		params.StartPort+DefaultWsPortOffset,
+		path,
+		params.NumberOfNodes,
+		int(params.AvgBlockDuration.Seconds()),
+		walletAddresses,
+	)
+
+	tmpHostConfig := config.HostConfig{
+		L1NodeHost:          Localhost,
+		L1NodeWebsocketPort: n.gethNetwork.WebSocketPorts[0],
+		L1ConnectionTimeout: DefaultL1ConnectionTimeout,
+	}
+	tmpEthClient, err := ethclient.NewEthClient(tmpHostConfig)
+	if err != nil {
+		panic(err)
+	}
+
+	mgmtContractAddr := deployContract(tmpEthClient, n.workerWallet, common.Hex2Bytes(mgmtcontractlib.MgmtContractByteCode))
+	erc20ContractAddr := deployContract(tmpEthClient, n.workerWallet, common.Hex2Bytes(erc20contract.ContractByteCode))
+
+	params.MgmtContractAddr = mgmtContractAddr
+	params.StableTokenContractAddr = erc20ContractAddr
+	params.MgmtContractLib = mgmtcontractlib.NewMgmtContractLib(mgmtContractAddr)
+	params.ERC20ContractLib = erc20contractlib.NewERC20ContractLib(mgmtContractAddr, erc20ContractAddr)
+
+	// Create the obscuro node, each connected to a geth node
+	l1Clients := make([]ethclient.EthClient, params.NumberOfNodes)
+	obscuroNodes := make([]*host.Node, params.NumberOfNodes)
+	n.obscuroClients = make([]*obscuroclient.Client, params.NumberOfNodes)
+
+	for i := 0; i < params.NumberOfNodes; i++ {
+		isGenesis := i == 0
+
+		// create the in memory l1 and l2 node
+		miner := createEthClientConnection(
+			int64(i),
+			n.gethNetwork.WebSocketPorts[i],
+		)
+		agg := createInMemObscuroNode(
+			int64(i),
+			isGenesis,
+			params.MgmtContractLib,
+			params.ERC20ContractLib,
+			params.AvgGossipPeriod,
+			params.AvgBlockDuration,
+			params.AvgNetworkLatency,
+			stats,
+			true,
+			n.gethNetwork.GenesisJSON,
+			params.NodeEthWallets[i],
+		)
+		obscuroClient := host.NewInMemObscuroClient(agg)
+
+		// and connect them to each other
+		agg.ConnectToEthNode(miner)
+
+		obscuroNodes[i] = agg
+		n.obscuroClients[i] = &obscuroClient
+		l1Clients[i] = miner
+	}
+
+	// make sure the aggregators can talk to each other
+	for i := 0; i < params.NumberOfNodes; i++ {
+		mockP2P := obscuroNodes[i].P2p.(*p2p.MockP2P)
+		mockP2P.Nodes = obscuroNodes
+	}
+
+	// start each obscuro node
+	for _, m := range obscuroNodes {
+		t := m
+		go t.Start()
+		time.Sleep(params.AvgBlockDuration / 10)
+	}
+
+	return l1Clients, n.obscuroClients, nil, nil
+}
+
+func (n *networkInMemGeth) TearDown() {
+	defer n.gethNetwork.StopNodes()
+	for _, client := range n.obscuroClients {
+		temp := client
+		go func() {
+			defer (*temp).Stop()
+			_ = (*temp).Call(nil, obscuroclient.RPCStopHost)
+		}()
+	}
+}
 
 func createEthClientConnection(id int64, port uint) ethclient.EthClient {
 	hostConfig := config.HostConfig{
