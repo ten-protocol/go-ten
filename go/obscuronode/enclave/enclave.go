248683d20651d7ad1d58fa69721e171f8600df24
Matt Curtis <matt@obscu.ro>
2022-06-10 17:31:10 +0100

    Use random temp file for sqlite persistence to avoid clash

--- go/obscuronode/enclave/enclave.go
+++ go/obscuronode/enclave/enclave.go
@@ -805,7 +805,8 @@ func getDB(nodeID uint64, cfg config.EnclaveConfig) (ethdb.Database, error) {
 	if !cfg.WillAttest {
 		// persistent but not secure in an enclave, we'll connect to a throwaway sqlite DB and test out persistence/sql implementations
 		nodecommon.LogWithID(nodeID, "Attestation is disabled, using a basic sqlite DB for persistence")
-		return sql.CreateTemporarySQLiteDB(nodeID)
+		// todo: for now we pass in an empty dbPath, when we want to test persistence after node restart we should change this to be config driven
+		return sql.CreateTemporarySQLiteDB("")
 	}
 
 	// persistent and with attestation means connecting to edgeless DB in a trusted enclave from a secure enclave
