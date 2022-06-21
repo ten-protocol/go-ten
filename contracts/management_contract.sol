bd4f07c1de3e04858905b290dcc2b675f3fc70f6
joeldudley <joel@obscu.ro>
2022-06-21 18:07:12 +0100

    Adds methods to management contract to store and retrieve host addresses.

--- contracts/management_contract.sol
+++ contracts/management_contract.sol
@@ -1,15 +1,13 @@
 // SPDX-License-Identifier: GPL-3.0
 import "libs/openzeppelin/cryptography/ECDSA.sol";
 
-
 pragma solidity >=0.7.0 <0.9.0;
 
 contract ManagementContract {
-
-    // TODO these should all be private
-    mapping(uint256 => Rollup[]) public rollups;
-    mapping(address => string) public attestationRequests;
-    mapping(address => bool) public attested;
+    mapping(uint256 => Rollup[]) private rollups;
+    mapping(address => string) private attestationRequests;
+    mapping(address => bool) private attested;
+    string[] private hostAddresses; // The addresses of all the Obscuro hosts on the network.
 
     // networkSecretNotInitialized marks if the network secret has been initialized
     bool private networkSecretInitialized ;
@@ -21,9 +19,8 @@ contract ManagementContract {
         uint256 Number;
     }
 
-
     function AddRollup(bytes32 ParentHash, address AggregatorID, bytes32 L1Block, uint256 Number, string calldata rollupData) public {
-        // TODO How to ensure the sender without hashing the calldata ?
+        // TODO How to ensure the sender without hashing the calldata?
         // bytes32 derp = keccak256(abi.encodePacked(ParentHash, AggregatorID, L1Block, Number, rollupData));
 
         // revert if the AggregatorID is not attested
@@ -67,7 +64,7 @@ contract ManagementContract {
 
         // todo remove this toAsciiString helper
         require(recoveredAddrSignedCalculated == attesterID,
-            string.concat("recovered address and attesterID dont match ",
+            string.concat("recovered address and attesterID don't match ",
                 "\n Expected:                         ", toAsciiString(attesterID),
                 "\n / recoveredAddrSignedCalculated:  ", toAsciiString(recoveredAddrSignedCalculated)));
 
@@ -75,21 +72,30 @@ contract ManagementContract {
         attested[requesterID] = true;
     }
 
+    function AddHostAddress(string memory hostAddress) public {
+        hostAddresses.push(hostAddress);
+    }
+
+    function GetHostAddresses() public view returns (string[] memory) {
+        return hostAddresses;
+    }
+
+    // Taken from https://ethereum.stackexchange.com/a/8447.
     function toAsciiString(address x) internal pure returns (string memory) {
         bytes memory s = new bytes(40);
         for (uint i = 0; i < 20; i++) {
             bytes1 b = bytes1(uint8(uint(uint160(x)) / (2**(8*(19 - i)))));
             bytes1 hi = bytes1(uint8(b) / 16);
             bytes1 lo = bytes1(uint8(b) - 16 * uint8(hi));
-            s[2*i] = char(hi);
-            s[2*i+1] = char(lo);
+            s[2*i] = toChar(hi);
+            s[2*i+1] = toChar(lo);
         }
         return string(s);
     }
 
-    function char(bytes1 b) internal pure returns (bytes1 c) {
+    // Taken from https://ethereum.stackexchange.com/a/8447.
+    function toChar(bytes1 b) internal pure returns (bytes1 c) {
         if (uint8(b) < 10) return bytes1(uint8(b) + 0x30);
         else return bytes1(uint8(b) + 0x57);
     }
 }
-
