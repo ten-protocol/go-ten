65fe0feff130ed5f960be6aace972f0e155a7146
joeldudley <joel@obscu.ro>
2022-07-11 10:13:58 +0100

    Removes unneeded formatting.

--- go/common/logging.go
+++ go/common/logging.go
@@ -9,29 +9,29 @@ import (
 // LogWithID logs a message at INFO level with the aggregator's identity prepended.
 func LogWithID(nodeID uint64, msg string, args ...interface{}) {
 	formattedMsg := fmt.Sprintf(msg, args...)
-	log.Info(fmt.Sprintf(">   Agg%d: %s", nodeID, formattedMsg))
+	log.Info(">   Agg%d: %s", nodeID, formattedMsg)
 }
 
 // WarnWithID logs a message at WARN level with the aggregator's identity prepended.
 func WarnWithID(nodeID uint64, msg string, args ...interface{}) {
 	formattedMsg := fmt.Sprintf(msg, args...)
-	log.Warn(fmt.Sprintf(">   Agg%d: %s", nodeID, formattedMsg))
+	log.Warn(">   Agg%d: %s", nodeID, formattedMsg)
 }
 
 // TraceWithID logs a message at TRACE level with the aggregator's identity prepended.
 func TraceWithID(nodeID uint64, msg string, args ...interface{}) {
 	formattedMsg := fmt.Sprintf(msg, args...)
-	log.Trace(fmt.Sprintf(">   Agg%d: %s", nodeID, formattedMsg))
+	log.Trace(">   Agg%d: %s", nodeID, formattedMsg)
 }
 
 // ErrorWithID logs a message at ERROR level with the aggregator's identity prepended.
 func ErrorWithID(nodeID uint64, msg string, args ...interface{}) {
 	formattedMsg := fmt.Sprintf(msg, args...)
-	log.Error(fmt.Sprintf(">   Agg%d: %s", nodeID, formattedMsg))
+	log.Error(">   Agg%d: %s", nodeID, formattedMsg)
 }
 
 // PanicWithID logs a message at PANIC level with the aggregator's identity prepended.
 func PanicWithID(nodeID uint64, msg string, args ...interface{}) {
 	formattedMsg := fmt.Sprintf(msg, args...)
-	log.Panic(fmt.Sprintf(">   Agg%d: %s", nodeID, formattedMsg))
+	log.Panic(">   Agg%d: %s", nodeID, formattedMsg)
 }
