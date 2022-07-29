1e9bcb4ba6df7e1c2f11700b28f5aef418bf9726
Matt Curtis <matt@obscu.ro>
2022-07-29 14:55:42 +0100

    add todo about hardcoded PKs

--- testnet/testnet-deploy-l2-contracts.sh
+++ testnet/testnet-deploy-l2-contracts.sh
@@ -29,6 +29,7 @@ testnet_path="${start_path}"
 
 # Define defaults
 l2port=13000
+# todo: get rid of these defaults and require them to be passed in, using github secrets for testnet values (requires bridge.go changes)
 obxpkstring="6e384a07a01263518a09a5424c7b6bbfc3604ba7d93f47e3a455cbdd7f9f0682"
 ethpkstring="4bfe14725e685901c062ccd4e220c61cf9c189897b6c78bd18d7f51291b2b8f8"
 
