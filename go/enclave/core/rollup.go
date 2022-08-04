d8648a7e87cde91eef7a962ec741139d3afe4f0e
Joel Dudley <joel@obscu.ro>
2022-08-04 15:31:58 +0100

    Sets time field in rollup headers. (#494)

--- go/enclave/core/rollup.go
+++ go/enclave/core/rollup.go
@@ -3,6 +3,7 @@ package core
 import (
 	"math/big"
 	"sync/atomic"
+	"time"
 
 	gethcommon "github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
@@ -44,6 +45,8 @@ func EmptyRollup(agg gethcommon.Address, parent *common.Header, blkHash gethcomm
 		L1Proof:     blkHash,
 		RollupNonce: nonce,
 		Number:      big.NewInt(int64(parent.Number.Uint64() + 1)),
+		// TODO - Consider how this time should align with the time of the L1 block used as proof.
+		Time: uint64(time.Now().Unix()),
 	}
 	r := Rollup{
 		Header: &h,
@@ -67,6 +70,7 @@ func NewRollup(blkHash gethcommon.Hash, parent *Rollup, height uint64, a gethcom
 		Number:      big.NewInt(int64(height)),
 		Withdrawals: withdrawals,
 		ReceiptHash: types.EmptyRootHash,
+		Time:        uint64(time.Now().Unix()),
 	}
 	r := Rollup{
 		Header:       &h,
