c145fe38a9cd87f586c59dd3930adb1d0c375809
joeldudley <joel@obscu.ro>
2022-08-04 10:55:49 +0100

    Adds (disabled) test.

--- go/host/hostrunner/cli_test.go
+++ go/host/hostrunner/cli_test.go
@@ -6,6 +6,7 @@ import (
 	"path"
 	"reflect"
 	"strconv"
+	"strings"
 	"testing"
 	"time"
 )
@@ -34,12 +35,14 @@ func TestConfigIsParsedFromCmdLineFlagsIfConfigFlagIsNotPresent(t *testing.T) {
 }
 
 func TestConfigFieldsMatchTomlConfigFields(t *testing.T) {
+	// We get all the config fields.
 	cfgReflection := reflect.TypeOf(config.HostConfig{})
 	cfgFields := make([]string, cfgReflection.NumField())
 	for i := 0; i < cfgReflection.NumField(); i++ {
 		cfgFields[i] = cfgReflection.Field(i).Name
 	}
 
+	// We get all the .toml config fields.
 	cfgTomlReflection := reflect.TypeOf(HostConfigToml{})
 	cfgTomlFields := make([]string, cfgTomlReflection.NumField())
 	for i := 0; i < cfgTomlReflection.NumField(); i++ {
@@ -48,5 +51,29 @@ func TestConfigFieldsMatchTomlConfigFields(t *testing.T) {
 
 	if !reflect.DeepEqual(cfgFields, cfgTomlFields) {
 		t.Fatalf("config file supports the following fields: %s, but .toml config file supports the following fields: %s", cfgFields, cfgTomlFields)
+	}
+}
+
+func TestConfigFlagsMatchConfigFields(t *testing.T) {
+	t.Skip("TODO - Reenable test when it's less disruptive to rename the CLI flags for consistency.")
+
+	// We get all the config fields.
+	cfgReflection := reflect.TypeOf(config.HostConfig{})
+	cfgFields := make([]string, cfgReflection.NumField())
+	for i := 0; i < cfgReflection.NumField(); i++ {
+		cfgFields[i] = strings.ToLower(cfgReflection.Field(i).Name)
+	}
+
+	// We get all the CLI flags via the usages map.
+	flagUsageMap := getFlagUsageMap()
+	i := 0
+	cliFlags := make([]string, len(flagUsageMap))
+	for key := range flagUsageMap {
+		cliFlags[i] = strings.ToLower(key)
+		i++
+	}
+
+	if !reflect.DeepEqual(cfgFields, cliFlags) {
+		t.Fatalf("config file supports the following fields: %s, but there are CLI flags for the following fields: %s", cfgFields, cliFlags)
 	}
 }
