27362687b2232766579a7eca5e475f5966bebf05
joeldudley <joel@obscu.ro>
2022-08-08 11:56:25 +0100

    Gofumpt-ing.

--- go/host/node/node.go
+++ go/host/node/node.go
@@ -4,10 +4,11 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"github.com/obscuronet/go-obscuro/go/host/rpc/clientapi"
 	"math/big"
 	"sync/atomic"
 	"time"
+
+	"github.com/obscuronet/go-obscuro/go/host/rpc/clientapi"
 
 	"github.com/obscuronet/go-obscuro/go/host"
 
