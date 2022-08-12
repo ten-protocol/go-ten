6799698470b375bc9e17ac5ea61186e678d62b33
Tudor Malene <tudor.malene@gmail.com>
2022-08-12 11:39:39 +0100

    protected erc20. version 2

--- integration/erc20contract/ObsERC20.sol
+++ integration/erc20contract/ObsERC20.sol
@@ -14,12 +14,37 @@ contract ObsERC20 is ERC20 {
     }
 
     function balanceOf(address account) public view virtual override returns (uint256) {
-        // Respond to balance requests made by the account owner only.
-        // In case the requester spoofs the "from" of the call, they will not be able to read
-        // the result since it will be returned encrypted with the viewing key of the declared "from".
-        // todo - figure out the correct condition to work with other contracts
-//        require(msg.sender == account);
-        return super.balanceOf(account);
+        // 1. Human owner of an account asking for the balance.
+        // 2. Human owner of an account interacting with a smart contract which in turn asks for the balance of the original asker.
+        // Note: In case the requester spoofs the "from" of the call, they will not be able to read
+        //  the result since it will be returned encrypted with the viewing key of the declared "from".
+        if (tx.origin == account) {
+            return super.balanceOf(account);
+        }
+
+        // 3. Contract asking for its own balance.
+        if (msg.sender == account) {
+            return super.balanceOf(account);
+        }
+
+        revert("Not allowed to read the balance");
     }
 
+    function allowance(address owner, address spender) public view virtual override returns (uint256) {
+        // 1. Human owner of an account asking for the allowance it has empowered someone to spend,
+        // or Human owner of an account asking for how much it is allowed to spend by someone else.
+        // 2. Human owner of an account interacting with a smart contract which in turn asks for the above
+        // Note: In case the requester spoofs the "from" of the call, they will not be able to read
+        //  the result since it will be returned encrypted with the viewing key of the declared "from".
+        if (tx.origin == owner || tx.origin == spender) {
+            return super.allowance(owner, spender);
+        }
+
+        // 3. Contract asking how much it is empowered to spend, or how much it has empowered someone else to spend.
+        if (msg.sender == owner || msg.sender == spender) {
+            return super.allowance(owner, spender);
+        }
+
+        revert("Not allowed to read the allowance");
+    }
 }
