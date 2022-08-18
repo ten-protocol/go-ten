ca486b09b10178909fe5eeb5084cff30189243b7
joeldudley <joel@obscu.ro>
2022-08-18 13:28:24 +0100

    Moves error from console to logs.

--- integration/gethnetwork/geth_network.go
+++ integration/gethnetwork/geth_network.go
@@ -430,7 +430,7 @@ func ensurePortsAreAvailable(startPort int, websocketStartPort int, numberNodes 
 func isPortAvailable(port int) bool {
 	ln, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
-		fmt.Printf("Listen port %d. Err: %s. ", port, err)
+		log.Error("Listen port %d. Err: %s. ", port, err)
 	}
 	if ln != nil {
 		_ = ln.Close()
