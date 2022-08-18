a023b35be355051641ffb877489aaca51e9ec5c1
Matt Curtis <matt@obscu.ro>
2022-08-18 13:29:02 +0100

    identify nonce-req failure in walext

--- integration/walletextension/wallet_extension_test.go
+++ integration/walletextension/wallet_extension_test.go
@@ -536,7 +536,7 @@ func sendTransactionAndAwaitConfirmation(txWallet wallet.Wallet, tx types.Legacy
 	nonceJSON := makeEthJSONReqAsJSON(rpcclientlib.RPCNonce, []interface{}{txWallet.Address().Hex(), latestBlock})
 	nonceString, ok := nonceJSON[walletextension.RespJSONKeyResult].(string)
 	if !ok {
-		panic(fmt.Errorf("retrieved nonce was not of type string"))
+		panic(fmt.Errorf("retrieved nonce was not of type string, resp: %s", nonceJSON))
 	}
 	nonce, err := hexutil.DecodeUint64(nonceString)
 	if err != nil {
