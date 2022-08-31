79333ac221ecc4e7c49375b6972c9169a2e7e968
joeldudley <joel@obscu.ro>
2022-08-31 16:24:37 +0100

    Better comments.

--- go/enclave/events/subscription_manager.go
+++ go/enclave/events/subscription_manager.go
@@ -19,7 +19,7 @@ func NewSubscriptionManager() *SubscriptionManager {
 	return &SubscriptionManager{}
 }
 
-// todo - describe
+// AddSubscription adds a log subscription to the enclave, provided the request is authenticated correctly.
 func (s *SubscriptionManager) AddSubscription(id uuid.UUID, subscription common.EncryptedLogSubscription) error {
 	// todo - decrypt and deserialize the subscription
 	subcription := logSubscription{}
@@ -33,32 +33,37 @@ func (s *SubscriptionManager) AddSubscription(id uuid.UUID, subscription common.
 	return nil
 }
 
-// todo - describe
+// DeleteSubscription removes a log subscription from the enclave.
+// TODO - Consider whether the deletion needs to be authenticated as well, to prevent attackers deleting subscriptions.
 func (s *SubscriptionManager) DeleteSubscription(id uuid.UUID) {
 	delete(s.subscriptions, id)
 }
 
-// todo - describe
+// FilterRelevantLogs returns only those logs for which one or more subscriptions exist.
 func (s *SubscriptionManager) FilterRelevantLogs(logs []*types.Log, stateDB *state.StateDB) map[uuid.UUID][]*types.Log {
 	relevantLogs := map[uuid.UUID][]*types.Log{}
+
 	for _, log := range logs {
 		for subID, sub := range s.subscriptions {
 			isRelevant, _ := sub.isRelevant(log, stateDB)
 			// todo return the account somehow, maybe as a tuple with the log
-			if isRelevant {
-				subResult, found := relevantLogs[subID]
-				if !found {
-					subResult = make([]*types.Log, 0)
-				}
-				subResult = append(subResult, log)
-				relevantLogs[subID] = subResult
+			if !isRelevant {
+				continue
+			}
+
+			logsForSubID, found := relevantLogs[subID]
+			if !found {
+				relevantLogs[subID] = []*types.Log{log}
+			} else {
+				relevantLogs[subID] = append(logsForSubID, log)
 			}
 		}
 	}
+
 	return relevantLogs
 }
 
-// todo - describe
+// EncryptLogs encrypts the logs with the corresponding viewing keys.
 func (s *SubscriptionManager) EncryptLogs(logsBySubID map[uuid.UUID][]*types.Log) (map[uuid.UUID]common.EncryptedLogs, error) {
 	result := map[uuid.UUID]common.EncryptedLogs{}
 	for subID, logs := range logsBySubID {
@@ -78,20 +83,17 @@ func (s *SubscriptionManager) EncryptLogs(logsBySubID map[uuid.UUID][]*types.Log
 	return result, nil
 }
 
+// Indicates whether the log is relevant for any subscription, and returns the matching subscription account if so.
+// A lifecycle log is considered relevant to everyone.
 func (s logSubscription) isRelevant(log *types.Log, db *state.StateDB) (bool, *subscriptionAccount) {
-	// todo
-	// transform the log into a useful data structure by extracting addresses from the log (according to the design)
-	// identify what type of log it is ( account specific or lifecycle log)
-	// if account-specific go through each SubscriptionAccount and check whether the log is relevant
-	// note - the above logic has to be reused to filter out the logs when someone requests a transaction receipt
-	// for logs that pass the above the step apply the filters
-	// return the first account for which the log matches, so it can be used for encryption
+	// todo - extract addresses from the logs
+	// todo - work out if this is an account or lifecycle log
+	// todo - if the former, establish whether it is relevant to any subscription
+	// todo - return the first account for which the log matches, so it can be used for encryption
 	return true, nil
 }
 
-// From the design - call must take a list of signed owning accounts.
-// Each account must be signed with the latest viewing key (to prevent someone from asking for random logs, just to leak info).
-// The call will fail if there are no viewing keys for all those accounts.
+// Information about a log subscription, including a list of authenticated accounts for the subscription.
 type logSubscription struct {
 	accounts []*subscriptionAccount
 	// todo Filters - the geth log filters
