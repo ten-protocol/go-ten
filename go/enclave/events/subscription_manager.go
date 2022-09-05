f2b3c13574087669242ed1ebc9449936213294e0
joeldudley <joel@obscu.ro>
2022-09-05 11:01:56 +0100

    Clean-up of todos.

--- go/enclave/events/subscription_manager.go
+++ go/enclave/events/subscription_manager.go
@@ -23,15 +23,13 @@ func NewSubscriptionManager() *SubscriptionManager {
 
 // AddSubscription adds a log subscription to the enclave under the given ID, provided the request is authenticated
 // correctly. If there is an existing subscription with the given ID, it is overwritten.
+// TODO - #453 - Decrypt subscriptions (currently expects unencrypted serialised bytes).
+// TODO - #453 - Check each account in the subscription request is authenticated with a signature.
 func (s *SubscriptionManager) AddSubscription(id uuid.UUID, encryptedSubscription common.EncryptedLogSubscription) error {
-	// TODO - #453 - Encryption of subscriptions.
-
 	var subscription common.LogSubscription
 	if err := rlp.DecodeBytes(encryptedSubscription, &subscription); err != nil {
 		return fmt.Errorf("could not decode encoded log subscription. Cause: %w", err)
 	}
-
-	// TODO - #453 - Check each account is signed.
 
 	s.subscriptions[id] = &subscription
 	println("jjj added subscription")
@@ -40,21 +38,21 @@ func (s *SubscriptionManager) AddSubscription(id uuid.UUID, encryptedSubscriptio
 
 // RemoveSubscription removes the log subscription with the given ID from the enclave. If there is no subscription with
 // the given ID, nothing is deleted.
-// TODO - Consider whether the deletion needs to be authenticated as well, to prevent attackers deleting subscriptions.
+// TODO - #453 - Consider whether the deletion needs to be authenticated as well, to prevent attackers deleting subscriptions.
 func (s *SubscriptionManager) RemoveSubscription(id uuid.UUID) {
 	delete(s.subscriptions, id)
 	println("jjj deleted subscription")
 }
 
 // FilterRelevantLogs returns only those logs for which one or more subscriptions exist.
+// TODO - #453 - Return which account each log is relevant to.
 func (s *SubscriptionManager) FilterRelevantLogs(logs []*types.Log, stateDB *state.StateDB) map[uuid.UUID][]*types.Log {
 	relevantLogs := map[uuid.UUID][]*types.Log{}
 
 	for _, log := range logs {
 		for subID, sub := range s.subscriptions {
-			isRelevant, _ := isRelevant(sub, log, stateDB)
-			// todo return the account somehow, maybe as a tuple with the log
-			if !isRelevant {
+			logIsRelevant, _ := isRelevant(sub, log, stateDB)
+			if !logIsRelevant {
 				continue
 			}
 
@@ -71,19 +69,19 @@ func (s *SubscriptionManager) FilterRelevantLogs(logs []*types.Log, stateDB *sta
 }
 
 // EncryptLogs encrypts the logs with the corresponding viewing keys.
+// TODO - #453 - Encrypt logs, rather than just serialising them.
 func (s *SubscriptionManager) EncryptLogs(logsBySubID map[uuid.UUID][]*types.Log) (map[uuid.UUID]common.EncryptedLogs, error) {
 	result := map[uuid.UUID]common.EncryptedLogs{}
 	for subID, logs := range logsBySubID {
 		enc := make([]byte, 0)
 		for _, log := range logs {
-			txReceiptBytes, err := log.MarshalJSON()
+			logBytes, err := log.MarshalJSON()
 			if err != nil {
 				return nil, fmt.Errorf("could not marshal log to JSON. Cause: %w", err)
 			}
 
-			// todo - add encryption
-			// todo - separator
-			enc = append(enc, txReceiptBytes...)
+			// TODO - #453 - Add separator between each serialised log.
+			enc = append(enc, logBytes...)
 		}
 		result[subID] = enc
 	}
@@ -92,10 +90,11 @@ func (s *SubscriptionManager) EncryptLogs(logsBySubID map[uuid.UUID][]*types.Log
 
 // Indicates whether the log is relevant for any subscription, and returns the matching subscription account if so.
 // A lifecycle log is considered relevant to everyone.
+// TODO - #453 - Filter logs, instead of considering all logs relevant to everyone.
 func isRelevant(sub *common.LogSubscription, log *types.Log, db *state.StateDB) (bool, *common.SubscriptionAccount) {
-	// todo - extract addresses from the logs
-	// todo - work out if this is an account or lifecycle log
-	// todo - if the former, establish whether it is relevant to any subscription
-	// todo - return the first account for which the log matches, so it can be used for encryption
+	// Extract addresses from the logs
+	// Work out if this is an account or lifecycle log
+	// If the former, establish whether it is relevant to any subscription
+	// Return the first account for which the log matches, so it can be used for encryption
 	return true, nil
 }
