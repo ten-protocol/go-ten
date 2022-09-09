f36fbc38ab7dc026bc5ed030a7e3a4ae4b0e350f
joeldudley <joel@obscu.ro>
2022-09-09 14:25:06 +0100

    Removes loop.

--- integration/walletextension/wallet_extension_test.go
+++ integration/walletextension/wallet_extension_test.go
@@ -305,8 +305,6 @@ func TestCanSubscribeForLogs(t *testing.T) {
 	createWalletExtension(t)
 
 	makeEthJSONReqAsJSON(rpc.RPCSubscribe, []interface{}{rpc.RPCSubscriptionTypeLogs, filters.FilterCriteria{}})
-
-	select {}
 }
 
 func TestCanDecryptSuccessfullyAfterSubmittingMultipleViewingKeys(t *testing.T) {
