0c35a06256e8cbf2c489fc1849e472e2fa8b0a91
joeldudley <joel@obscu.ro>
2022-09-12 11:21:25 +0100

    Address review comment.

--- tools/walletextension/readwriter/read_writer.go
+++ tools/walletextension/readwriter/read_writer.go
@@ -38,14 +38,16 @@ type WSReadWriter struct {
 }
 
 func NewReadWriter(resp http.ResponseWriter, req *http.Request) (ReadWriter, error) {
+	// We search all the request's headers. If there's a websocket upgrade header, we upgrade to a websocket connection.
 	for _, header := range req.Header[headerUpgrade] {
 		if header == headerWebsocket {
 			conn, err := upgrader.Upgrade(resp, req, nil)
 			if err != nil {
 				err = fmt.Errorf("unable to upgrade to websocket connection")
-				logAndSendErr(resp, err.Error())
+				httpLogAndSendErr(resp, err.Error())
 				return nil, err
 			}
+
 			return &WSReadWriter{
 				conn: conn,
 			}, nil
@@ -55,7 +57,7 @@ func NewReadWriter(resp http.ResponseWriter, req *http.Request) (ReadWriter, err
 	return &HTTPReadWriter{resp: resp, req: req}, nil
 }
 
-func (h HTTPReadWriter) ReadRequest() ([]byte, error) {
+func (h *HTTPReadWriter) ReadRequest() ([]byte, error) {
 	body, err := io.ReadAll(h.req.Body)
 	if err != nil {
 		return nil, fmt.Errorf("could not read request body: %w", err)
@@ -63,7 +65,7 @@ func (h HTTPReadWriter) ReadRequest() ([]byte, error) {
 	return body, nil
 }
 
-func (h HTTPReadWriter) WriteResponse(msg []byte) error {
+func (h *HTTPReadWriter) WriteResponse(msg []byte) error {
 	_, err := h.resp.Write(msg)
 	if err != nil {
 		return fmt.Errorf("could not write response: %w", err)
@@ -71,31 +73,32 @@ func (h HTTPReadWriter) WriteResponse(msg []byte) error {
 	return nil
 }
 
-func (h HTTPReadWriter) HandleError(msg string) {
-	logAndSendErr(h.resp, msg)
+func (h *HTTPReadWriter) HandleError(msg string) {
+	httpLogAndSendErr(h.resp, msg)
 }
 
-func (h HTTPReadWriter) SupportsSubscriptions() bool {
+func (h *HTTPReadWriter) SupportsSubscriptions() bool {
 	return false
 }
 
-func (w WSReadWriter) ReadRequest() ([]byte, error) {
+func (w *WSReadWriter) ReadRequest() ([]byte, error) {
 	panic("not implemented")
 }
 
-func (w WSReadWriter) WriteResponse(msg []byte) error {
+func (w *WSReadWriter) WriteResponse(msg []byte) error {
 	panic("not implemented")
 }
 
-func (w WSReadWriter) HandleError(msg string) {
+func (w *WSReadWriter) HandleError(msg string) {
 	panic("not implemented")
 }
 
-func (w WSReadWriter) SupportsSubscriptions() bool {
+func (w *WSReadWriter) SupportsSubscriptions() bool {
 	return true
 }
 
-func logAndSendErr(resp http.ResponseWriter, msg string) {
+// Logs the error, prints it to the console, and returns the error over HTTP.
+func httpLogAndSendErr(resp http.ResponseWriter, msg string) {
 	log.Error(msg)
 	fmt.Println(msg)
 	http.Error(resp, msg, httpCodeErr)
