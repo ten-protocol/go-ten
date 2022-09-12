ee23f2d1b4b9e4262c254e47bfa9d8590426d186
joeldudley <joel@obscu.ro>
2022-09-12 12:21:45 +0100

    Fix wallet extension.

--- tools/walletextension/main/cli.go
+++ tools/walletextension/main/cli.go
@@ -40,8 +40,8 @@ func parseCLIArgs() walletextension.Config {
 
 	return walletextension.Config{
 		WalletExtensionPort:     *walletExtensionPort,
-		NodeRPCHTTPAddress:      fmt.Sprintf("%s:%d", *nodeHost, *nodeHTTPPort),
-		NodeRPCWebsocketAddress: fmt.Sprintf("%s:%d", *nodeHost, *nodeWebsocketPort),
+		NodeRPCHTTPAddress:      fmt.Sprintf("http://%s:%d", *nodeHost, *nodeHTTPPort),
+		NodeRPCWebsocketAddress: fmt.Sprintf("ws://%s:%d", *nodeHost, *nodeWebsocketPort),
 		LogPath:                 *logPath,
 	}
 }
