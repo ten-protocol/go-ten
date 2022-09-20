777f07d1e8586e92ac15faf894437b39fc4e09d8
Pedro Gomes <otherview@gmail.com>
2022-09-20 15:37:14 +0100

    lint

--- tools/walletextension/accountmanager/account_manager.go
+++ tools/walletextension/accountmanager/account_manager.go
@@ -5,14 +5,11 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"math/big"
 	"strings"
 	"time"
 
 	"github.com/obscuronet/go-obscuro/go/common/gethenconding"
 
-	"github.com/ethereum/go-ethereum"
-	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/obscuronet/go-obscuro/go/common/log"
 	"github.com/obscuronet/go-obscuro/go/rpc"
@@ -321,66 +318,4 @@ func (r *RPCRequest) Clone() *RPCRequest {
 		Method: r.Method,
 		Params: r.Params,
 	}
-}
-
-// convertToCallMsg converts the interface to a *ethereum.CallMsg
-func convertToCallMsg(callMsgInterface interface{}) (*ethereum.CallMsg, error) {
-	// geth lowercases the field name and uses the last seen value
-	var toString, fromString, dataString, valueString string
-	var to, from gethcommon.Address
-	var data []byte
-	var value *big.Int
-	var ok bool
-	var err error
-	for field, val := range callMsgInterface.(map[string]interface{}) {
-		switch strings.ToLower(field) {
-		case CallFieldTo:
-			toString, ok = val.(string)
-			if !ok {
-				return nil, fmt.Errorf("unexpected type supplied in `to` field")
-			}
-			to = gethcommon.HexToAddress(toString)
-		case CallFieldFrom:
-			fromString, ok = val.(string)
-			if !ok {
-				return nil, fmt.Errorf("unexpected type supplied in `from` field")
-			}
-			from = gethcommon.HexToAddress(fromString)
-		case CallFieldData:
-			dataString, ok = val.(string)
-			if !ok {
-				return nil, fmt.Errorf("unexpected type supplied in `data` field")
-			}
-
-			// data can be nil
-			if len(dataString) > 0 {
-				data, err = hexutil.Decode(dataString)
-				if err != nil {
-					return nil, fmt.Errorf("could not decode data in CallMsg - %w", err)
-				}
-			}
-		case CallFieldValue:
-			valueString, ok = val.(string)
-			if !ok {
-				return nil, fmt.Errorf("unexpected type supplied in `value` field")
-			}
-			value, err = hexutil.DecodeBig(valueString)
-			if err != nil {
-				return nil, fmt.Errorf("could not decode value in CallMsg - %w", err)
-			}
-		}
-	}
-
-	// convert the params[0] into an ethereum.CallMsg
-	return &ethereum.CallMsg{
-		From:       from,
-		To:         &to,
-		Gas:        0,
-		GasPrice:   nil,
-		GasFeeCap:  nil,
-		GasTipCap:  nil,
-		Value:      value,
-		Data:       data,
-		AccessList: nil,
-	}, nil
 }
