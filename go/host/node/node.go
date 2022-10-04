08a82b12d47de5efbc5c7b83e06ac5ee7c4abd08
joeldudley <joel@obscu.ro>
2022-10-04 10:57:11 +0100

    Plumbs in latestSeenRollup field.

--- go/host/node/node.go
+++ go/host/node/node.go
@@ -67,15 +67,12 @@ type Node struct {
 	forkRPCCh    chan []common.EncodedBlock // The channel that new forks from the L1 node are sent to
 	rollupsP2PCh chan common.EncodedRollup  // The channel that new rollups from peers are sent to
 	txP2PCh      chan common.EncryptedTx    // The channel that new transactions from peers are sent to
-	logsChs      map[rpc.ID]chan []byte     // The channels that logs are sent to, one per subscription
 
 	nodeDB *db.DB // Stores the node's publicly-available data
 
-	// library to handle Management Contract lib operations
-	mgmtContractLib mgmtcontractlib.MgmtContractLib
-
-	// Wallet used to issue ethereum transactions
-	ethWallet wallet.Wallet
+	mgmtContractLib  mgmtcontractlib.MgmtContractLib // Library to handle Management Contract lib operations
+	ethWallet        wallet.Wallet                   // Wallet used to issue ethereum transactions
+	logSubscriptions map[rpc.ID]logSubscription      // The channels that logs are sent to, one per subscription
 }
 
 func NewHost(
@@ -110,16 +107,14 @@ func NewHost(
 		forkRPCCh:    make(chan []common.EncodedBlock),
 		rollupsP2PCh: make(chan common.EncodedRollup),
 		txP2PCh:      make(chan common.EncryptedTx),
-		logsChs:      map[rpc.ID]chan []byte{},
 
 		// Initialize the node DB
 		// nodeDB:       NewLevelDBBackedDB(), // todo - make this config driven
 		nodeDB: db.NewInMemoryDB(),
 
-		// library that provides a handler for Management Contract
-		mgmtContractLib: mgmtContractLib,
-		// the nodes ethereum wallet
-		ethWallet: ethWallet,
+		mgmtContractLib:  mgmtContractLib, // library that provides a handler for Management Contract
+		ethWallet:        ethWallet,       // the node's ethereum wallet
+		logSubscriptions: map[rpc.ID]logSubscription{},
 	}
 
 	if config.HasClientRPCHTTP || config.HasClientRPCWebsockets {
@@ -325,12 +320,12 @@ func (a *Node) ReceiveTx(tx common.EncryptedTx) {
 	a.txP2PCh <- tx
 }
 
-func (a *Node) Subscribe(id rpc.ID, encryptedLogSubscription common.EncryptedParamsLogSubscription, matchedLogs chan []byte) error {
+func (a *Node) Subscribe(id rpc.ID, encryptedLogSubscription common.EncryptedParamsLogSubscription, matchedLogsCh chan []byte) error {
 	err := a.EnclaveClient().Subscribe(id, encryptedLogSubscription)
 	if err != nil {
 		return fmt.Errorf("could not create subscription with enclave. Cause: %w", err)
 	}
-	a.logsChs[id] = matchedLogs
+	a.logSubscriptions[id] = logSubscription{ch: matchedLogsCh}
 	return nil
 }
 
@@ -339,7 +334,11 @@ func (a *Node) Unsubscribe(id rpc.ID) {
 	if err != nil {
 		log.Error("could not terminate subscription %s with enclave. Cause: %s", id, err)
 	}
-	delete(a.logsChs, id)
+	logSubscription, found := a.logSubscriptions[id]
+	if found {
+		close(logSubscription.ch)
+		delete(a.logSubscriptions, id)
+	}
 }
 
 func (a *Node) Stop() {
@@ -611,7 +610,7 @@ func (a *Node) storeBlockProcessingResult(result common.BlockSubmissionResponse)
 func (a *Node) sendLogsToSubscribers(result common.BlockSubmissionResponse) {
 	for subscriptionID, encLogsByRollup := range result.SubscribedLogs {
 		for _, encryptedLogs := range encLogsByRollup {
-			a.logsChs[subscriptionID] <- encryptedLogs
+			a.logSubscriptions[subscriptionID].ch <- encryptedLogs
 		}
 	}
 }
@@ -1062,4 +1061,10 @@ func retryWithBackoff(tries int, funcToRetry func() error) error {
 		time.Sleep(pause)
 		pause = pause * 2
 	}
+}
+
+// Pairs the latest seen rollup for a log subscription with the channel on which new logs should be sent.
+type logSubscription struct {
+	latestSeenRollup uint64      // The latest rollup for which logs have been distributed to this subscription.
+	ch               chan []byte // The channel that logs for this subscription are sent to.
 }
