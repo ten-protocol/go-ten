80e803d7531bf3f1119a477c6f110c10cee16e9d
Matt Curtis <matt@obscu.ro>
2022-10-30 22:34:25 +0000

    Wait for receipts to confirm L1 transactions

--- go/common/retry/retry_strategy.go
+++ go/common/retry/retry_strategy.go
@@ -2,6 +2,7 @@ package retry
 
 import (
 	"fmt"
+	"math"
 	"time"
 )
 
@@ -14,6 +15,7 @@ type Strategy interface {
 	Reset()                           // reset is called before the first attempt is made, can be used for recording start time or setting attempts to zero
 }
 
+// NewTimeoutStrategy retries at the provided (fixed) interval until the timeout duration has elapsed
 func NewTimeoutStrategy(timeout time.Duration, interval time.Duration) Strategy {
 	return &timeoutStrategy{
 		timeout:  timeout,
@@ -47,4 +49,44 @@ func (t *timeoutStrategy) Summary() string {
 func (t *timeoutStrategy) Reset() {
 	t.attempts = 0
 	t.startTime = time.Now()
+}
+
+// NewDoublingBackoffStrategy keeps retrying until successful or until it reaches maxRetries, doubling the initialInterval
+// wait period after each additional retry to avoid exacerbating problems with failing traffic
+func NewDoublingBackoffStrategy(initialInterval time.Duration, maxRetries uint64) Strategy {
+	return &backoffStrategy{
+		initialInterval:        initialInterval,
+		maxRetries:             maxRetries,
+		intervalIncreaseFactor: 2,
+	}
+}
+
+type backoffStrategy struct {
+	maxRetries             uint64
+	initialInterval        time.Duration
+	intervalIncreaseFactor float64
+
+	startTime time.Time
+	attempts  uint64
+}
+
+func (b *backoffStrategy) NextRetryInterval() time.Duration {
+	b.attempts++
+	return b.initialInterval * time.Duration(math.Pow(b.intervalIncreaseFactor, float64(b.attempts-1)))
+}
+
+func (b *backoffStrategy) Done() bool {
+	return b.attempts >= b.maxRetries
+}
+
+func (b *backoffStrategy) Summary() string {
+	if b.Done() {
+		return fmt.Sprintf("completed maximum permitted retries (%d) over %s", b.attempts, time.Since(b.startTime))
+	}
+	return fmt.Sprintf("retrying after %d attempts", b.attempts)
+}
+
+func (b *backoffStrategy) Reset() {
+	b.attempts = 0
+	b.startTime = time.Now()
 }
