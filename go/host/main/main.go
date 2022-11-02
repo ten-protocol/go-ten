c5aa8be8b2eb0c429cf3e12f78ab24ab8353ec13
Tudor Malene <tudor.malene@gmail.com>
2022-11-02 14:00:03 +0000

    fix parsing

--- go/host/main/main.go
+++ go/host/main/main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"bytes"
 	"fmt"
-
 	gethcommon "github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/status-im/keycard-go/hexutils"
+	"strings"
 
 	"github.com/obscuronet/go-obscuro/go/host/hostrunner"
 )
@@ -18,14 +18,23 @@ func main() {
 		panic(fmt.Errorf("could not parse config. Cause: %w", err))
 	}
 	addr := toAddress(config.PrivateKeyString)
-	if config.PKAddress != "" && !bytes.Equal(hexutils.HexToBytes(config.PKAddress), addr.Bytes()) {
+	pkadd := config.PKAddress
+	if strings.HasPrefix(config.PKAddress, "0x") {
+		pkadd = config.PKAddress[2:]
+	}
+
+	if config.PKAddress != "" && !bytes.Equal(hexutils.HexToBytes(pkadd), addr.Bytes()) {
 		fmt.Printf("WARN: the address: %s does not match the private key %s\n", config.PKAddress, config.PrivateKeyString)
 	}
 	hostrunner.RunHost(config)
 }
 
 func toAddress(privateKey string) gethcommon.Address {
-	privateKeyA, err := crypto.ToECDSA(hexutils.HexToBytes(privateKey))
+	k := privateKey
+	if strings.HasPrefix(privateKey, "0x") {
+		k = privateKey[2:]
+	}
+	privateKeyA, err := crypto.ToECDSA(hexutils.HexToBytes(k))
 	if err != nil {
 		panic(err)
 	}
