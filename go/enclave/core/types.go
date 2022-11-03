edaa2dbf0e0f1f6435afc72c3f62cff41954f931
joeldudley <joel@obscu.ro>
2022-11-03 16:06:50 +0000

    Self-review.

--- go/enclave/core/types.go
+++ go/enclave/core/types.go
@@ -4,7 +4,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 )
 
-// BlockState pairs a block with the rollup it contains.
+// BlockState pairs a block with the canonical head rollup and the rollup the block contains, if any.
 type BlockState struct {
 	Block          common.Hash
 	HeadRollup     common.Hash // The head rollup of the canonical L2 chain.
