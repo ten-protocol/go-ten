65b36a2f87e9d3bad0b0bbdde11d58885fb3bbde
joeldudley <joel@obscu.ro>
2022-11-17 13:24:16 +0000

    .

--- go/host/db/batches.go
+++ go/host/db/batches.go
@@ -9,7 +9,6 @@ import (
 	gethcommon "github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/rlp"
 	"github.com/obscuronet/go-obscuro/go/common"
-	"github.com/obscuronet/go-obscuro/go/common/log"
 )
 
 // DB methods relating to batches.
@@ -26,7 +25,10 @@ func (db *DB) GetHeadBatchHeader() (*common.Header, error) {
 // AddBatchHeader adds a batch's header to the known headers
 func (db *DB) AddBatchHeader(header *common.Header, txHashes []common.TxHash) error {
 	b := db.kvStore.NewBatch()
-	db.writeBatchHeader(header)
+	err := db.writeBatchHeader(header)
+	if err != nil {
+		return err
+	}
 
 	// TODO - #718 - Store the batch txs, batch hash, and batch number per transaction hash, if needed (see `AddRollupHeader`).
 
@@ -38,11 +40,14 @@ func (db *DB) AddBatchHeader(header *common.Header, txHashes []common.TxHash) er
 		return err
 	}
 	if errors.Is(err, errutil.ErrNotFound) || headBatchHeader.Number.Int64() <= header.Number.Int64() {
-		db.writeHeadBatchHash(header.Hash())
+		err = db.writeHeadBatchHash(header.Hash())
+		if err != nil {
+			return err
+		}
 	}
 
 	if err := b.Write(); err != nil {
-		db.logger.Crit("Could not write batch.", log.ErrKey, err)
+		return err
 	}
 
 	return nil
@@ -57,21 +62,21 @@ func batchHeaderKey(hash gethcommon.Hash) []byte {
 func (db *DB) readBatchHeader(hash gethcommon.Hash) (*common.Header, error) {
 	f, err := db.kvStore.Has(batchHeaderKey(hash))
 	if err != nil {
-		db.logger.Crit("could not retrieve batch header.", log.ErrKey, err)
+		return nil, err
 	}
 	if !f {
 		return nil, errutil.ErrNotFound
 	}
 	data, err := db.kvStore.Get(batchHeaderKey(hash))
 	if err != nil {
-		db.logger.Crit("could not retrieve batch header.", log.ErrKey, err)
+		return nil, err
 	}
 	if len(data) == 0 {
 		return nil, errutil.ErrNotFound
 	}
 	header := new(common.Header)
 	if err := rlp.Decode(bytes.NewReader(data), header); err != nil {
-		db.logger.Crit("could not decode batch header.", log.ErrKey, err)
+		return nil, err
 	}
 	return header, nil
 }
@@ -80,36 +85,38 @@ func (db *DB) readBatchHeader(hash gethcommon.Hash) (*common.Header, error) {
 func (db *DB) readHeadBatchHash() (*gethcommon.Hash, error) {
 	f, err := db.kvStore.Has(headBatch)
 	if err != nil {
-		db.logger.Crit("could not retrieve head batch.", log.ErrKey, err)
+		return nil, err
 	}
 	if !f {
 		return nil, errutil.ErrNotFound
 	}
 	value, err := db.kvStore.Get(headBatch)
 	if err != nil {
-		db.logger.Crit("could not retrieve head batch.", log.ErrKey, err)
+		return nil, err
 	}
 	h := gethcommon.BytesToHash(value)
 	return &h, nil
 }
 
 // Stores a batch header into the database.
-func (db *DB) writeBatchHeader(header *common.Header) {
+func (db *DB) writeBatchHeader(header *common.Header) error {
 	// Write the encoded header
 	data, err := rlp.EncodeToBytes(header)
 	if err != nil {
-		db.logger.Crit("could not encode batch header.", log.ErrKey, err)
+		return err
 	}
 	key := batchHeaderKey(header.Hash())
 	if err := db.kvStore.Put(key, data); err != nil {
-		db.logger.Crit("could not put batch header in DB.", log.ErrKey, err)
+		return err
 	}
+	return nil
 }
 
 // Stores the head batch header hash into the database.
-func (db *DB) writeHeadBatchHash(val gethcommon.Hash) {
+func (db *DB) writeHeadBatchHash(val gethcommon.Hash) error {
 	err := db.kvStore.Put(headBatch, val.Bytes())
 	if err != nil {
-		db.logger.Crit("could not put head batch hash in DB.", log.ErrKey, err)
+		return err
 	}
+	return nil
 }
--- go/host/db/blocks.go
+++ go/host/db/blocks.go
@@ -10,16 +10,15 @@ import (
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethdb"
 	"github.com/ethereum/go-ethereum/rlp"
-	"github.com/obscuronet/go-obscuro/go/common/log"
 )
 
 // DB methods relating to batches.
 
 // GetHeadBlockHeader returns the block header of the current head block.
 func (db *DB) GetHeadBlockHeader() (*types.Header, error) {
-	head := db.readHeadBlock(db.kvStore)
-	if head == nil {
-		return nil, errutil.ErrNotFound
+	head, err := db.readHeadBlock(db.kvStore)
+	if err != nil {
+		return nil, err
 	}
 	return db.readBlockHeader(db.kvStore, *head)
 }
@@ -32,7 +31,10 @@ func (db *DB) GetBlockHeader(hash gethcommon.Hash) (*types.Header, error) {
 // AddBlockHeader adds a types.Header to the known headers
 func (db *DB) AddBlockHeader(header *types.Header) error {
 	b := db.kvStore.NewBatch()
-	db.writeBlockHeader(header)
+	err := db.writeBlockHeader(header)
+	if err != nil {
+		return err
+	}
 
 	// update the head if the new height is greater than the existing one
 	headBlockHeader, err := db.GetHeadBlockHeader()
@@ -40,11 +42,14 @@ func (db *DB) AddBlockHeader(header *types.Header) error {
 		return err
 	}
 	if errors.Is(err, errutil.ErrNotFound) || headBlockHeader.Number.Int64() <= header.Number.Int64() {
-		db.writeHeadBlockHash(header.Hash())
+		err := db.writeHeadBlockHash(header.Hash())
+		if err != nil {
+			return err
+		}
 	}
 
 	if err := b.Write(); err != nil {
-		db.logger.Crit("Could not write rollup.", log.ErrKey, err)
+		return err
 	}
 
 	return nil
@@ -56,60 +61,62 @@ func blockHeaderKey(hash gethcommon.Hash) []byte {
 }
 
 // Stores a block header into the database
-func (db *DB) writeBlockHeader(header *types.Header) {
+func (db *DB) writeBlockHeader(header *types.Header) error {
 	// Write the encoded header
 	data, err := rlp.EncodeToBytes(header)
 	if err != nil {
-		db.logger.Crit("could not encode block header.", log.ErrKey, err)
+		return err
 	}
 	key := blockHeaderKey(header.Hash())
 	if err := db.kvStore.Put(key, data); err != nil {
-		db.logger.Crit("could not put header in DB.", log.ErrKey, err)
+		return err
 	}
+	return nil
 }
 
-func (db *DB) writeHeadBlockHash(val gethcommon.Hash) {
+func (db *DB) writeHeadBlockHash(val gethcommon.Hash) error {
 	err := db.kvStore.Put(headBlock, val.Bytes())
 	if err != nil {
-		db.logger.Crit("could not write head block.", log.ErrKey, err)
+		return err
 	}
+	return nil
 }
 
 // Retrieves the block header corresponding to the hash.
 func (db *DB) readBlockHeader(r ethdb.KeyValueReader, hash gethcommon.Hash) (*types.Header, error) {
 	f, err := r.Has(blockHeaderKey(hash))
 	if err != nil {
-		db.logger.Crit("could not retrieve block header.", log.ErrKey, err)
+		return nil, err
 	}
 	if !f {
 		return nil, errutil.ErrNotFound
 	}
 	data, err := r.Get(blockHeaderKey(hash))
 	if err != nil {
-		db.logger.Crit("could not retrieve block header.", log.ErrKey, err)
+		return nil, err
 	}
 	if len(data) == 0 {
 		return nil, errutil.ErrNotFound
 	}
 	header := new(types.Header)
 	if err := rlp.Decode(bytes.NewReader(data), header); err != nil {
-		db.logger.Crit("could not decode block header.", log.ErrKey, err)
+		return nil, err
 	}
 	return header, nil
 }
 
-func (db *DB) readHeadBlock(r ethdb.KeyValueReader) *gethcommon.Hash {
+func (db *DB) readHeadBlock(r ethdb.KeyValueReader) (*gethcommon.Hash, error) {
 	f, err := r.Has(headBlock)
 	if err != nil {
-		db.logger.Crit("could not retrieve head block.", log.ErrKey, err)
+		return nil, err
 	}
 	if !f {
-		return nil
+		return nil, errutil.ErrNotFound
 	}
 	value, err := r.Get(headBlock)
 	if err != nil {
-		db.logger.Crit("could not retrieve head block.", log.ErrKey, err)
+		return nil, err
 	}
 	h := gethcommon.BytesToHash(value)
-	return &h
+	return &h, nil
 }
