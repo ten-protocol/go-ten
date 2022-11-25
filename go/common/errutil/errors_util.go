6d1166895bf4795b5a9cc462460e68045adc9780
Joel <joel@obscu.ro>
2022-11-25 12:36:30 +0000

    Make our ErrNotFound match Ethereum's (#885)

--- go/common/errutil/errors_util.go
+++ go/common/errutil/errors_util.go
@@ -1,8 +1,14 @@
 package errutil
 
-import "errors"
+import (
+	"errors"
+
+	"github.com/ethereum/go-ethereum"
+)
 
 var (
-	ErrNotFound = errors.New("not found")
+	// ErrNotFound must equal Geth's not-found error. This is because some Geth components we use throw the latter, and
+	// we want to be able to catch both types in a single error-check.
+	ErrNotFound = ethereum.NotFound
 	ErrNoImpl   = errors.New("not implemented")
 )
