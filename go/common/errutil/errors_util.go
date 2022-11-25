ac921fa0001807d2dd96de2511f99d23fea9c15e
joeldudley <joel@obscu.ro>
2022-11-25 12:35:38 +0000

    .

--- go/common/errutil/errors_util.go
+++ go/common/errutil/errors_util.go
@@ -7,6 +7,8 @@ import (
 )
 
 var (
+	// ErrNotFound must equal Geth's not-found error. This is because some Geth components we use throw the latter, and
+	// we want to be able to catch both types in a single error-check.
 	ErrNotFound = ethereum.NotFound
 	ErrNoImpl   = errors.New("not implemented")
 )
