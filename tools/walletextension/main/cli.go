5a4943c37ee781a14a816026915a577cad9c93df
joeldudley <joel@obscu.ro>
2022-12-06 23:03:49 +0000

    Revert change.

--- tools/walletextension/main/cli.go
+++ tools/walletextension/main/cli.go
@@ -18,7 +18,7 @@ const (
 	walletExtensionPortWSUsage   = "The port on which to serve websocket JSON RPC requests. Default: 3001."
 
 	nodeHostName    = "nodeHost"
-	nodeHostDefault = "localhost"
+	nodeHostDefault = "testnet.obscu.ro"
 	nodeHostUsage   = "The host on which to connect to the Obscuro node. Default: `testnet.obscu.ro`."
 
 	nodeHTTPPortName    = "nodePortHTTP"
