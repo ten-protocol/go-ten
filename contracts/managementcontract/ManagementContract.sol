119c44c316d354889442f7d40353f67c4c15eec8
StefanIliev545 <siliev@Stefans-MacBook-Pro.local>
2022-12-12 15:35:06 +0200

    Added OpenZeppelin v4.7.0 entire repository.

--- contracts/managementcontract/ManagementContract.sol
+++ contracts/managementcontract/ManagementContract.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-import "../libs/openzeppelin/cryptography/ECDSA.sol";
+import "../libs/openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 pragma solidity >=0.7.0 <0.9.0;
 
