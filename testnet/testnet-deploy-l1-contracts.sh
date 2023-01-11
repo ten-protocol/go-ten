4e003815e5a209d948f7551c501916c3cd2aeb24
StefanIliev545 <siliev@Stefans-MacBook-Pro.local>
2023-01-11 18:44:07 +0100

    Exporting env variables.

--- testnet/testnet-deploy-l1-contracts.sh
+++ testnet/testnet-deploy-l1-contracts.sh
@@ -76,3 +76,11 @@ docker run --name=hh-l1-deployer \
     "${docker_image}" \
     deploy \
     --network layer1
+
+mgmtContractAddr=$(docker logs --tail 5 hh-l1-deployer | grep -e 'ManagementContract' | cut -c 121-162)
+pocErc20Addr=$(docker logs --tail 5 hh-l1-deployer | grep -e 'deploying "POC' | cut -c 111-152)
+hocErc20Addr=$(docker logs --tail 5 hh-l1-deployer | grep -e 'deploying "HOC' | cut -c 111-152)
+
+echo "MGMTCONTRACTADDR=${mgmtContractAddr}" > "${testnet_path}/.env"
+echo "HOCERC20ADDR=${hocErc20Addr}" >> "${testnet_path}/.env"
+echo "POCERC20ADDR=${pocErc20Addr}" >> "${testnet_path}/.env"
