c2263b9286941a31eb31ad6d167887e4d6aa13b7
StefanIliev545 <siliev@Stefans-MacBook-Pro.local>
2023-01-11 14:38:12 +0100

    Some reshuffling of folder structure, new hardhatdeployer tool dockerfile.

--- contracts/deployment_scripts/testnet/001_deploy_tokens.ts
+++ contracts/deployment_scripts/testnet/001_deploy_tokens.ts
@@ -9,14 +9,32 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
     const {deployer} = await getNamedAccounts();
 
+    await deployments.deploy('HOCERC20', {
+        from: deployer,
+        contract: "WrappedERC20",
+        args: [ "HOC", "HOC" ],
+        log: true,
+    });
+
     await deployments.deploy('POCERC20', {
         from: deployer,
         contract: "WrappedERC20",
         args: [ "POC", "POC" ],
         log: true,
     });
+
+
+    await deployments.execute('HOCERC20', {
+        from: deployer,
+        log: true,
+    }, "issueFor", deployer, "1000000000000000000000000000000");
+
+    await deployments.execute('POCERC20', {
+        from: deployer,
+        log: true, 
+    }, "issueFor", deployer, "1000000000000000000000000000000");
 };
 
 export default func;
-func.tags = ['POCERC20', 'POCERC20_deploy'];
+func.tags = ['HPERC20', 'HPERC20_deploy'];
 func.dependencies = ['ObscuroBridge'];
