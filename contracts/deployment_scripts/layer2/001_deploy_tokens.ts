442b7641ce6e064ed7292e0a6720a646f75bba3b
StefanIliev545 <siliev@Stefans-MacBook-Pro.local>
2023-01-16 17:04:14 +0100

    Fixed to work.

--- contracts/deployment_scripts/layer2/001_deploy_tokens.ts
+++ contracts/deployment_scripts/layer2/001_deploy_tokens.ts
@@ -12,27 +12,28 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     await deployments.deploy('L2HOCERC20', {
         from: deployer,
         contract: "WrappedERC20",
-        args: [ "HOC", "HOC", "1000000000000000000000000000000" ],
-        log: true,
+        args: [ "HOC", "HOC" ],
+        log: true
     });
 
     await deployments.deploy('L2POCERC20', {
         from: deployer,
         contract: "WrappedERC20",
         args: [ "POC", "POC" ],
-        log: true,
+        log: true
     });
 
     await deployments.execute('L2HOCERC20', {
-        from: deployer
+        from: deployer,
+        log: true
     }, "issueFor", deployer, "1000000000000000000000000000000");
 
 
     await deployments.execute('L2POCERC20', {
-        from: deployer
+        from: deployer,
+        log: true
     }, "issueFor", deployer, "1000000000000000000000000000000");
 };
 
 export default func;
 func.tags = ['L2HPERC20', 'L2HPERC20_deploy'];
-func.dependencies = ['ObscuroBridge'];
