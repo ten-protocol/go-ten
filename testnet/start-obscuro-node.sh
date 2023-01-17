c22566eafd78b306d3677c9a99d2ec282f8e956e
StefanIliev545 <siliev@Stefans-MacBook-Pro.local>
2023-01-17 05:48:46 +0100

    Additional stuff for fully working deployment.

--- testnet/start-obscuro-node.sh
+++ testnet/start-obscuro-node.sh
@@ -75,6 +75,7 @@ host_id=0x0654D8B60033144D567f25bF41baC1FB0D60F23B
 pk_string=8ead642ca80dadb0f346a66cd6aa13e08a8ac7b5c6f7578d4bac96f5db01ac99
 log_level=4
 sequencer_id=0x0654D8B60033144D567f25bF41baC1FB0D60F23B
+msg_bus_addr=0xFD03804faCA2538F4633B3EBdfEfc38adafa259B
 
 
 # Fetch options
@@ -101,6 +102,7 @@ do
             --dev_testnet)              dev_testnet=${value} ;;
             --sequencerID)              sequencer_id=${value} ;;
             --pccs_addr)                pccs_addr=${value} ;;
+            --msg_bus_addr)             msg_bus_addr=${value} ;;
 
             --help)                     help_and_exit ;;
             *)
@@ -130,6 +132,7 @@ echo "LOGLEVEL=${log_level}" >> "${testnet_path}/.env"
 echo "PROFILERENABLED=${profiler_enabled}" >> "${testnet_path}/.env"
 echo "P2PPUBLICADDRESS=${p2p_public_address}" >> "${testnet_path}/.env"
 echo "SEQUENCERID=${sequencer_id}" >> "${testnet_path}/.env"
+echo "MSGBUSCONTRACTADDR=${msg_bus_addr}" >> "${testnet_path}/.env"
 
 # only set the env var if it's defined
 if [ -z ${pccs_addr+x} ];
