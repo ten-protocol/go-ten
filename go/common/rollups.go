72c062b8a20388ea912e27b04b4d72402631794b
Joel <joel@obscu.ro>
2023-01-20 15:39:11 +0000

    Requires the full set of batches to be specified when creating a rollup from a set of batches (#1056)

--- go/common/rollups.go
+++ go/common/rollups.go
@@ -1,6 +1,7 @@
 package common
 
 import (
+	"sort"
 	"sync/atomic"
 )
 
@@ -21,4 +22,18 @@ func (r *ExtRollup) Hash() L2RootHash {
 	v := r.Header.Hash()
 	r.hash.Store(v)
 	return v
+}
+
+// ExtRollupFromExtBatches converts a set of ExtBatch into an ExtRollup. The hash of the head batch (the one with the
+// highest number) is stored in the ExtRollup header's `HeadBatchHash` field.
+func ExtRollupFromExtBatches(batches []*ExtBatch) *ExtRollup {
+	sort.Slice(batches, func(i, j int) bool {
+		// Ascending order sort.
+		return batches[i].Header.Number.Cmp(batches[j].Header.Number) < 0
+	})
+
+	return &ExtRollup{
+		Header:  batches[len(batches)-1].Header.ToRollupHeader(),
+		Batches: batches,
+	}
 }
