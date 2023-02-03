275ad9b1497b5a278debe643468f0f94c2c4fad0
Matt Curtis <matt@obscu.ro>
2023-02-03 22:33:17 +0000

    disable full-network-test for CI until it is working

--- integration/networktest/tests/ci/full_network_sim_test.go
+++ integration/networktest/tests/ci/full_network_sim_test.go
@@ -11,6 +11,8 @@ import (
 
 // TestFullNetworkSim reimplements the test case of our current "sim" tests (the ERC20 transaction injector)
 func TestFullNetworkSim(t *testing.T) {
+	// todo: once this test is fully functional, remove this line so the test runs regularly in CI builds
+	networktest.TestOnlyRunsInIDE(t)
 	// todo: this is a placeholder, need to add more validation etc. to achieve parity with the existing full network sim
 	networktest.Run(t, env.LocalDevNetwork(), traffic.DurationTest(traffic.ERC20Transfers(), 30*time.Second))
 }
