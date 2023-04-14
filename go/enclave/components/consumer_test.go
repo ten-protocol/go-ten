832a8f31df7743fa5c70367824def4582ccdd54e
StefanIliev545 <siliev@Stefans-MacBook-Pro.local>
2023-04-15 00:42:09 +0200

    Added skip for a test.

--- go/enclave/components/consumer_test.go
+++ go/enclave/components/consumer_test.go
@@ -11,6 +11,8 @@ import (
 )
 
 func TestInvalidBlocksAreRejected(t *testing.T) {
+	t.Skipf("skipping test that relied on l1blockchain previously which hasn't been used in a while")
+
 	// todo - how does this test even work, storage is never set and we attempt to fetch head block?
 	blockConsumer := blockConsumer{}
 
