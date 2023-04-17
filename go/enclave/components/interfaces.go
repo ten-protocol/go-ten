1421bfdb315d870733a4fc2238f73a633617739d
StefanIliev545 <siliev@Stefans-MacBook-Pro.local>
2023-04-17 15:08:36 +0100

    Added comments to interfaces.

--- go/enclave/components/interfaces.go
+++ go/enclave/components/interfaces.go
@@ -10,8 +10,6 @@ import (
 	"github.com/obscuronet/go-obscuro/go/enclave/core"
 )
 
-type BlockStatus uint16
-
 type BlockIngestionType struct {
 	// IsLatest is true if this block was the canonical head of the L1 chain at the time it was submitted to enclave
 	// (if false then we are behind and catching up, expect to be fed another block immediately afterwards)
@@ -26,11 +24,6 @@ type BlockIngestionType struct {
 	// genesis block and then, we should only see one block ingested in a 'PreGenesis' state)
 	PreGenesis bool
 }
-
-const (
-	Latest = BlockStatus(0)
-	Fork   = BlockStatus(1)
-)
 
 type BlockConsumer interface {
 	ConsumeBlock(br *common.BlockAndReceipts, isLatest bool) (*BlockIngestionType, error)
@@ -48,48 +41,82 @@ type BatchContext struct {
 	ChainConfig  *params.ChainConfig
 }
 
+// ComputedBatch - a structure representing the result of a batch
+// computation where `Batch` is the newly computed batch and `Receipts`
+// are the receipts for the executed transactions inside this batch.
+// The `Commit` function allows for committing the stateDB resulting from
+// the computation of the batch. One might not want to commit in case the
+// resulting batch differs than what is being validated for example.
 type ComputedBatch struct {
 	Batch    *core.Batch
 	Receipts types.Receipts
 	Commit   func(bool) (gethcommon.Hash, error)
 }
 type BatchProducer interface {
+
+	// ComputeBatch - will formulate a batch and execute the transactions according to the
+	// state provided in the BatchContext.
 	// Call with same BatchContext should always produce identical extBatch - idempotent
 	// Should be safe to call in parallel
 	ComputeBatch(*BatchContext) (*ComputedBatch, error)
+
+	// CreateGenesisState - will create and commit the genesis state in the stateDB for the given block hash,
+	// sequencer address and uint64 timestamp representing the time now. In this genesis state is where one can
+	// find preallocated funds for faucet. TODO - make this an option
 	CreateGenesisState(common.L1BlockHash, common.L2Address, uint64) (*core.Batch, *types.Transaction, error)
 }
 
 type BatchRegistry interface {
+	// StoreBatch - will store the batch and receipts in storage.
+	// Furthermore any heads and pointers would be updated here and
+	// after all is done the batch will be pushed to the subscribers
+	// in order to update them.
 	StoreBatch(*core.Batch, types.Receipts) error
 
+	// GetHeadBatch - Returns the batch considered to be the L2 head.
 	GetHeadBatch() (*core.Batch, error)
+
+	// GetHeadBatchFor - Returns the head batch for given L1 block.
+	// Note that this does not seek backwards so if a given block does not have
+	// any pointers to batches then `ErrNotFound` will be returned.
 	GetHeadBatchFor(common.L1BlockHash) (*core.Batch, error)
+
+	// GetBatch - Given a batch hash returns the matching batch taken from the
+	// storage. If no such batch is found then nil and `ErrNotFound` should
+	// be returned.
 	GetBatch(common.L2BatchHash) (*core.Batch, error)
 
+	// FindAncestralBatchFor - this function is akin to `GetHeadBatchFor`, but
+	// it does not check the block passed as a parameter, but rather starts from its
+	// parent block and then seeks backwards until a head batch is found or genesis is reached.
 	FindAncestralBatchFor(*common.L1Block) (*core.Batch, error)
+
+	// BatchesAfter - Given a hash, will return batches following it until the head batch
 	BatchesAfter(batchHash gethcommon.Hash) ([]*core.Batch, error)
 
+	// GetBatchStateAtHeight - creates a stateDB that represents the state committed when
+	// the batch with height matching the blockNumber was created and stored.
 	GetBatchStateAtHeight(blockNumber *gethrpc.BlockNumber) (*state.StateDB, error)
+
+	// GetBatchStateAtHeight - same as `GetBatchStateAtHeight`, but instead returns the full batch
+	// rather than its stateDB only.
 	GetBatchAtHeight(height gethrpc.BlockNumber) (*core.Batch, error)
 
+	// Subscribe - creates and returns a channel that will be used to push any newly created batches
+	// to the subscriber.
 	Subscribe() chan *core.Batch
+	// Unsubscribe - informs the registry that the subscriber is no longer listening, allowing it to
+	// gracefully terminate any streaming and stop queueing new batches.
 	Unsubscribe()
 
+	// HasGenesisBatch - returns if genesis batch is available yet or not, or error in case
+	// the function is unable to determine.
 	HasGenesisBatch() (bool, error)
 }
 
-func allReceipts(txReceipts []*types.Receipt, depositReceipts []*types.Receipt) types.Receipts {
-	return append(txReceipts, depositReceipts...)
-}
-
-type sortByTxIndex []*types.Receipt
-
-func (c sortByTxIndex) Len() int           { return len(c) }
-func (c sortByTxIndex) Swap(i, j int)      { c[i], c[j] = c[j], c[i] }
-func (c sortByTxIndex) Less(i, j int) bool { return c[i].TransactionIndex < c[j].TransactionIndex }
-
 type RollupProducer interface {
+	// CreateRollup - creates a rollup starting from the end of the last rollup
+	// that has been stored and continues it towards what we consider the current L2 head.
 	CreateRollup() (*core.Rollup, error)
 }
 
@@ -99,3 +126,14 @@ type RollupConsumer interface {
 	// not been seenp previously.
 	ProcessL1Block(b *common.BlockAndReceipts) ([]*core.Rollup, error)
 }
+
+// TODO - find a better place for the following stuff
+func allReceipts(txReceipts []*types.Receipt, depositReceipts []*types.Receipt) types.Receipts {
+	return append(txReceipts, depositReceipts...)
+}
+
+type sortByTxIndex []*types.Receipt
+
+func (c sortByTxIndex) Len() int           { return len(c) }
+func (c sortByTxIndex) Swap(i, j int)      { c[i], c[j] = c[j], c[i] }
+func (c sortByTxIndex) Less(i, j int) bool { return c[i].TransactionIndex < c[j].TransactionIndex }
--- go/enclave/services/interfaces.go
+++ go/enclave/services/interfaces.go
@@ -6,20 +6,37 @@ import (
 	"github.com/obscuronet/go-obscuro/go/enclave/core"
 )
 
+// ObscuroService - the interface for any service type running in Obscuro nodes.
+// Should only contain the shared functionality that every service type needs to have.
 type ObscuroService interface {
-	//	SubmitTx() //todo
+	//	ReceiveBlock - function that accepts L1 blocks and their receipts along with a flag
+	// that signals if the block is the latest one or not. Processing of those blocks and
+	// resulting actions differ between the service types.
 	ReceiveBlock(*common.BlockAndReceipts, bool) (*components.BlockIngestionType, error)
+
+	// SubmitTransaction - L2 obscuro transactions need to be passed here. Sequencers
+	// will put them in the mempool while validators might put them in a queue and monitor
+	// for censorship.
 	SubmitTransaction(*common.L2Tx) error
 }
 
 type Sequencer interface {
+	// CreateBatch - creates a new head batch for the parameter block or if there is no block
+	// provided, for the latest known L1 head block.
 	CreateBatch(*common.L1Block) (*core.Batch, error)
+
+	// CreateRollup - creates a new rollup from the latest recorded rollup in the head l1 chain
+	// and adds as many batches to it as possible.
 	CreateRollup() (*common.ExtRollup, error)
 
 	ObscuroService
 }
 
 type ObsValidator interface {
+	// ValidateAndStoreBatch - if all the prerequisites are available (parent batch, l1 block) then
+	// this function recomputes the batch using the exact same context and compares the results.
+	// If the batch is valid it will be stored.
 	ValidateAndStoreBatch(*core.Batch) error
+
 	ObscuroService
 }
