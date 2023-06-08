6fbeca2d220ecf12f23719479e5612dd76799469
StefanIliev545 <siliev@Stefans-MacBook-Pro.local>
2023-06-08 20:06:01 +0300

    Added comment explaining.

--- go/common/measure/measure.go
+++ go/common/measure/measure.go
@@ -6,6 +6,10 @@ type Stopwatch struct {
 	start time.Time
 }
 
+// NewStopwatch creates a stopwatch that simply holds starting time.
+// The idea behind its usage is to have its String() function redirect to
+// the function that measures elapsed time since its creation in order to plug it into
+// defered logger calls.
 func NewStopwatch() *Stopwatch {
 	return &Stopwatch{
 		start: time.Now(),
