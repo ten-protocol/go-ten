5d524409021112b9bb223f0eba17aff589b9d7f0
Tudor Malene <tudor.malene@gmail.com>
2023-06-26 15:47:50 +0100

    simplify block processing logic (#1359)

--- go/enclave/nodetype/validator.go
+++ go/enclave/nodetype/validator.go
@@ -1,14 +1,12 @@
 package nodetype
 
 import (
-	"errors"
 	"fmt"
 
 	gethcommon "github.com/ethereum/go-ethereum/common"
 	gethlog "github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/params"
 	"github.com/obscuronet/go-obscuro/go/common"
-	"github.com/obscuronet/go-obscuro/go/common/log"
 	"github.com/obscuronet/go-obscuro/go/enclave/components"
 	"github.com/obscuronet/go-obscuro/go/enclave/core"
 	"github.com/obscuronet/go-obscuro/go/enclave/db"
@@ -61,29 +59,6 @@ func (val *obsValidator) ValidateAndStoreBatch(incomingBatch *core.Batch) error 
 	}
 
 	return val.batchRegistry.StoreBatch(incomingBatch, receipts)
-}
-
-func (val *obsValidator) ReceiveBlock(br *common.BlockAndReceipts, isLatest bool) (*components.BlockIngestionType, error) {
-	ingestion, err := val.blockProcessor.Process(br, isLatest)
-	if err != nil {
-		return nil, err
-	}
-
-	rollup, err := val.rollupConsumer.ProcessL1Block(br)
-	if err != nil && !errors.Is(err, components.ErrDuplicateRollup) {
-		// todo - log err?
-		val.logger.Error("Encountered error processing l1 block", log.ErrKey, err)
-		return ingestion, nil
-	}
-
-	if rollup != nil {
-		// read batch data from rollup, verify and store it
-		if err = val.rollupConsumer.ProcessRollup(rollup); err != nil {
-			return nil, err
-		}
-	}
-
-	return ingestion, nil
 }
 
 func (val *obsValidator) SubmitTransaction(transaction *common.L2Tx) error {
