86e0152ea6d47521adce4aeb6cad834a1e93062d
Žiga Kokelj <ziga.kokelj@gmail.com>
2023-08-02 17:13:27 +0200

    allow enclave to produce handlers for both old and new message types (#1417)

--- go/enclave/vkhandler/vk_handler.go
+++ go/enclave/vkhandler/vk_handler.go
@@ -25,9 +25,10 @@ type VKHandler struct {
 // New creates a new viewing key handler
 // checks if the signature is valid
 // as well if signature matches account address
+// todo (@ziga) - function now accepts both old and new messages
 func New(requestedAddr *gethcommon.Address, vkPubKeyBytes, accountSignatureHexBytes []byte) (*VKHandler, error) {
 	// Recalculate the message signed by MetaMask.
-	msgToSign := viewingkey.GenerateSignMessage(vkPubKeyBytes)
+	msgToSign := viewingkey.GenerateSignMessageOG(vkPubKeyBytes, requestedAddr)
 
 	// We recover the key based on the signed message and the signature.
 	recoveredAccountPublicKey, err := crypto.SigToPub(accounts.TextHash([]byte(msgToSign)), accountSignatureHexBytes)
@@ -36,8 +37,19 @@ func New(requestedAddr *gethcommon.Address, vkPubKeyBytes, accountSignatureHexBy
 	}
 	recoveredAccountAddress := crypto.PubkeyToAddress(*recoveredAccountPublicKey)
 
+	// We recover the key based on the signed message and the signature (same as before, but with legacy message format "vk"+<vk>"
+	// todo (@ziga) remove this once old WE message format is deprecated
+	msgToSignLegacy := viewingkey.GenerateSignMessage(vkPubKeyBytes)
+	recoveredAccountPublicKeyLegacy, err := crypto.SigToPub(accounts.TextHash([]byte(msgToSignLegacy)), accountSignatureHexBytes)
+	if err != nil {
+		return nil, fmt.Errorf("viewing key but could not validate its signature - %w", err)
+	}
+	recoveredAccountAddressLegacy := crypto.PubkeyToAddress(*recoveredAccountPublicKeyLegacy)
+
 	// is the requested account address the same as the address recovered from the signature
-	if requestedAddr.Hash() != recoveredAccountAddress.Hash() {
+	// todo (@ziga) - we currently check also for legacy address and allow both (remove this after transition period)
+	if requestedAddr.Hash() != recoveredAccountAddress.Hash() &&
+		requestedAddr.Hash() != recoveredAccountAddressLegacy.Hash() {
 		return nil, ErrInvalidAddressSignature
 	}
 
