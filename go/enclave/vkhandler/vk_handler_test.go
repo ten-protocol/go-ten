a7a82b1020110c2241548341741bdab6c28f2ac8
Žiga Kokelj <ziga.kokelj@gmail.com>
2023-08-02 13:19:16 +0200

    allow enclave to produce handlers for both old and new message types

--- go/enclave/vkhandler/vk_handler_test.go
+++ go/enclave/vkhandler/vk_handler_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestVKHandler(t *testing.T) {
+func TestVKHandlerWEMessageFormat(t *testing.T) {
 	// generate user private Key
 	userPrivKey, err := crypto.GenerateKey()
 	if err != nil {
@@ -34,7 +34,64 @@ func TestVKHandler(t *testing.T) {
 	assert.NoError(t, err)
 }
 
-func TestSignAndCheckSignature(t *testing.T) {
+func TestVKHandlerOGMessageFormat(t *testing.T) {
+	// generate user private Key
+	userPrivKey, err := crypto.GenerateKey()
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	userAddr := crypto.PubkeyToAddress(userPrivKey.PublicKey)
+
+	// generate ViewingKey private Key
+	vkPrivKey, err := crypto.GenerateKey()
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	vkPubKeyBytes := crypto.CompressPubkey(ecies.ImportECDSAPublic(&vkPrivKey.PublicKey).ExportECDSA())
+
+	// Sign key
+	msgToSign := viewingkey.GenerateSignMessageOG(vkPubKeyBytes, &userAddr)
+	signature, err := crypto.Sign(accounts.TextHash([]byte(msgToSign)), userPrivKey)
+	assert.NoError(t, err)
+
+	// Create a new vk Handler
+	_, err = New(&userAddr, vkPubKeyBytes, signature)
+	assert.NoError(t, err)
+}
+
+func TestSignAndCheckSignatureOGMessageFormat(t *testing.T) {
+	// generate user private Key
+	userPrivKey, err := crypto.GenerateKey()
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	userAddr := crypto.PubkeyToAddress(userPrivKey.PublicKey)
+
+	// generate ViewingKey private Key
+	vkPrivKey, err := crypto.GenerateKey()
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	vkPubKeyByte := crypto.CompressPubkey(ecies.ImportECDSAPublic(&vkPrivKey.PublicKey).ExportECDSA())
+	// sign the message
+	msgToSign := viewingkey.GenerateSignMessageOG(vkPubKeyByte, &userAddr)
+	signature, err := crypto.Sign(accounts.TextHash([]byte(msgToSign)), userPrivKey)
+	assert.NoError(t, err)
+
+	// Recover the key based on the signed message and the signature.
+	recoveredAccountPublicKey, err := crypto.SigToPub(accounts.TextHash([]byte(msgToSign)), signature)
+	assert.NoError(t, err)
+	recoveredAccountAddress := crypto.PubkeyToAddress(*recoveredAccountPublicKey)
+
+	if recoveredAccountAddress.Hex() != userAddr.Hex() {
+		t.Errorf("unable to recover user address from signature")
+	}
+
+	_, err = crypto.DecompressPubkey(vkPubKeyByte)
+	assert.NoError(t, err)
+}
+
+func TestSignAndCheckSignatureWEMessageFormat(t *testing.T) {
 	// generate user private Key
 	userPrivKey, err := crypto.GenerateKey()
 	if err != nil {
