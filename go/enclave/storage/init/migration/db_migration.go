7edbacff02912a0c19391e87a0c33d27cb3fc8d9
Tudor Malene <tudor.malene@gmail.com>
2023-08-23 14:33:58 +0300

    add very primitive db migration mechanism.

---
+++ go/enclave/storage/init/migration/db_migration.go
@@ -0,0 +1,124 @@
+package migration
+
+import (
+	"database/sql"
+	"embed"
+	"errors"
+	"io/fs"
+	"sort"
+	"strconv"
+	"strings"
+	"unsafe"
+
+	gethlog "github.com/ethereum/go-ethereum/log"
+	"github.com/obscuronet/go-obscuro/go/common/errutil"
+	"github.com/obscuronet/go-obscuro/go/enclave/storage/enclavedb"
+)
+
+const currentFileKey = "CURRENT_FILE"
+
+func DBMigration(db *sql.DB, sqlFiles embed.FS, logger gethlog.Logger) error {
+	migrationFiles, err := readMigrationFiles(sqlFiles)
+	if err != nil {
+		return err
+	}
+
+	maxMigration := int64(len(migrationFiles))
+
+	var maxDB int64
+	config, err := enclavedb.FetchConfig(db, currentFileKey)
+	if err != nil {
+		// first time there is no entry, so we assume 001 was executed already
+		if errors.Is(err, errutil.ErrNotFound) {
+			maxDB = 1
+		} else {
+			return err
+		}
+	} else {
+		maxDB = ByteArrayToInt(config)
+	}
+
+	// write to the database
+	for i := maxDB; i < maxMigration; i++ {
+		logger.Info("Executing db migration", "file", migrationFiles[i].Name())
+		content, err := sqlFiles.ReadFile(migrationFiles[i].Name())
+		if err != nil {
+			return err
+		}
+		err = executeMigration(db, string(content), i)
+		if err != nil {
+			return err
+		}
+		logger.Info("Successfully executed", "file", migrationFiles[i].Name(), "index", i)
+	}
+
+	return nil
+}
+
+func executeMigration(db *sql.DB, content string, migrationOrder int64) error {
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+	_, err = tx.Exec(content)
+	if err != nil {
+		return err
+	}
+
+	_, err = enclavedb.WriteConfigToTx(tx, currentFileKey, IntToByteArray(migrationOrder))
+	if err != nil {
+		return err
+	}
+
+	return tx.Commit()
+}
+
+func readMigrationFiles(sqlFiles embed.FS) ([]fs.DirEntry, error) {
+	migrationFiles, err := sqlFiles.ReadDir(".")
+	if err != nil {
+		return nil, err
+	}
+
+	// sort the migrationFiles based on the prefix (before "_")
+	sort.Slice(migrationFiles, func(i, j int) bool {
+		// Extract the number prefix and compare
+		return getPrefix(migrationFiles[i]) < getPrefix(migrationFiles[j])
+	})
+
+	// sanity check the consecutive rule
+	for i, file := range migrationFiles {
+		prefix := getPrefix(file)
+		if i+1 != prefix {
+			panic("Invalid migration file. Missing index")
+		}
+	}
+	return migrationFiles, err
+}
+
+func getPrefix(migrationFile fs.DirEntry) int {
+	prefix := strings.Split(migrationFile.Name(), "_")[0]
+	number, err := strconv.Atoi(prefix)
+	if err != nil {
+		panic("Invalid db migration file")
+	}
+	return number
+}
+
+func IntToByteArray(num int64) []byte {
+	size := int(unsafe.Sizeof(num))
+	arr := make([]byte, size)
+	for i := 0; i < size; i++ {
+		byt := *(*uint8)(unsafe.Pointer(uintptr(unsafe.Pointer(&num)) + uintptr(i)))
+		arr[i] = byt
+	}
+	return arr
+}
+
+func ByteArrayToInt(arr []byte) int64 {
+	val := int64(0)
+	size := len(arr)
+	for i := 0; i < size; i++ {
+		*(*uint8)(unsafe.Pointer(uintptr(unsafe.Pointer(&val)) + uintptr(i))) = arr[i]
+	}
+	return val
+}
