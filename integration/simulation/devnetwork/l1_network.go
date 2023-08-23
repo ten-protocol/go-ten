9dd3d581576be8c162da468e4a31f7ae5b89a920
Matt Curtis <matt@obscu.ro>
2023-08-23 14:34:47 +0100

    fix comment

--- integration/simulation/devnetwork/l1_network.go
+++ integration/simulation/devnetwork/l1_network.go
@@ -8,7 +8,7 @@ import (
 // (it could be a local geth docker network, a local in-memory network or even a live public L1)
 // todo (@matt) - refactor to use the same NodeOperator approach as the L2?
 type L1Network interface {
-	Prepare() // ensure L1 connectivity (start nodes if required) and deploy Obscuro network contracts
+	Prepare() // ensure L1 connectivity (start nodes if required)
 	CleanUp() // shut down  nodes if required, clean up connections
 	NumNodes() int
 	GetClient(i int) ethadapter.EthClient
