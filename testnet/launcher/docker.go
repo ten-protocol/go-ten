344effedb8dd353ef951396dd8c668cef11b9530
otherview <otherview@gmail.com>
2023-09-05 12:49:43 +0100

    nit

--- testnet/launcher/docker.go
+++ testnet/launcher/docker.go
@@ -61,6 +61,7 @@ func (t *Testnet) Start() error {
 		node.WithL1Start(networkConfig.L1StartHash),
 		node.WithInMemoryHostDB(true),
 		node.WithDebugNamespaceEnabled(true),
+		node.WithInboundP2PEnabled(true),
 		node.WithLogLevel(4),
 	)
 
