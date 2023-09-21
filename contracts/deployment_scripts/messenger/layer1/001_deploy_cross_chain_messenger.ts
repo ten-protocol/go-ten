4f92616f7d464b42aacf74831c65c469a9bbb475
StefanIliev545 <stefan@obscu.ro>
2023-09-21 15:02:30 +0300

    Added deployment script.

--- contracts/deployment_scripts/messenger/layer1/001_deploy_cross_chain_messenger.ts
+++ contracts/deployment_scripts/messenger/layer1/001_deploy_cross_chain_messenger.ts
@@ -14,7 +14,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const { deployer } = await hre.companionNetworks.layer1.getNamedAccounts();
 
     // Read the message bus address from the management contract deployment.
-    const messageBusAddress : string = process.env.MESSAGE_BUS_ADDRESS || "0xa1fdA5f6Df55a326f5f4300F3A716317f0f03110"
+    const messageBusAddress : string = process.env.MESSAGE_BUS_ADDRESS || "0x3FA8A7A039519602eBA00D443a14C0eb2358359a"
     console.log(`Message Bus address ${messageBusAddress}`);
 
     // Setup the cross chain messenger and point it to the message bus from the management contract to be used for validation
@@ -27,4 +27,4 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
 export default func;
 func.tags = ['CrossChainMessenger', 'CrossChainMessenger_deploy'];
-func.dependencies = ['ManagementContract', 'HPERC20']; //TODO: Remove HPERC20, this is only to have matching addresses.
+func.dependencies = ['ManagementContract', 'HPERC20', 'GasPrefunding']; //TODO: Remove HPERC20, this is only to have matching addresses.
