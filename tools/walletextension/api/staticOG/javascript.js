48fbb757e09be4d98087c51ebd0aee35bc00e4de
Cais <50658567+CaisManai@users.noreply.github.com>
2023-10-31 17:12:20 +0000

    Update javascript.js

--- tools/walletextension/api/staticOG/javascript.js
+++ tools/walletextension/api/staticOG/javascript.js
@@ -278,21 +278,6 @@ async function switchToObscuroNetwork() {
     return -1
 }
 
-function copyFaucetRequest() {
-    // Get the text field
-    var copyText = document.getElementById("myInput");
-  
-    // Select the text field
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); // For mobile devices
-  
-     // Copy the text inside the text field
-    navigator.clipboard.writeText(copyText.value);
-  
-    // Alert the copied text
-    alert("Copied the text: " + copyText.value);
-  }
-
 const initialize = async () => {
     const joinButton = document.getElementById(idJoin);
     const moreInfoButton = document.getElementById(idMoreInfo);
