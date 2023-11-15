9f17cc0370cc5686e7a4d250789805b17145d3ea
Jennie <echenimjennifer@gmail.com>
2023-11-15 19:46:49 +0400

    update tailwind config

--- tools/obscuroscan_v3/frontend/tailwind.config.ts
+++ tools/obscuroscan_v3/frontend/tailwind.config.ts
@@ -5,7 +5,7 @@ const config: Config = {
   darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
