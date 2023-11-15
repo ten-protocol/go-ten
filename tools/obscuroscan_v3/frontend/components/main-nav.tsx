05f867cb0c0d612e43fdbc2bcb6f0e84136eeb8d
Jennie <echenimjennifer@gmail.com>
2023-11-15 18:16:54 +0400

    chore: use tree structure for main nav

--- tools/obscuroscan_v3/frontend/components/main-nav.tsx
+++ tools/obscuroscan_v3/frontend/components/main-nav.tsx
@@ -12,70 +12,84 @@ import {
 } from "./ui/dropdown-menu";
 
 import { ChevronDownIcon } from "@radix-ui/react-icons";
-import { NavLinks } from "@/src/routes";
 import { NavLink } from "@/src/types/interfaces";
+import { useRouter } from "next/router";
 
-export function MainNav({
-  className,
-  ...props
-}: React.HTMLAttributes<HTMLElement>) {
-  return (
-    <nav
-      className={cn("flex items-center space-x-4 lg:space-x-6", className)}
-      {...props}
-    >
-      {NavLinks.map((navLink: NavLink, index: number) => {
-        return navLink.isDropdown ? (
-          <DropdownMenu key={index}>
-            <DropdownMenuTrigger asChild>
-              <Button
-                variant="clear"
-                className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary p-0"
-              >
-                {navLink.label} <ChevronDownIcon className="ml-1 h-4 w-4" />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className="w-56" align="end" forceMount>
-              <DropdownMenuGroup>
-                {navLink.subNavLinks &&
-                  navLink.subNavLinks.map((subNavLink: NavLink) =>
-                    subNavLink.isExternal ? (
-                      <a href={subNavLink.href} key={subNavLink.label}>
-                        <DropdownMenuItem>{subNavLink.label}</DropdownMenuItem>
-                      </a>
-                    ) : (
-                      subNavLink.href && (
-                        <Link href={subNavLink.href} key={subNavLink.label}>
-                          <DropdownMenuItem className="cursor-pointer">
-                            {subNavLink.label}
-                          </DropdownMenuItem>
-                        </Link>
-                      )
-                    )
-                  )}
-              </DropdownMenuGroup>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        ) : navLink.isExternal ? (
-          <a
-            key={index}
-            href={navLink.href}
-            className="text-sm font-medium transition-colors hover:text-primary"
-          >
-            {navLink.label}
-          </a>
-        ) : (
-          navLink.href && (
-            <Link
-              key={index}
-              href={navLink.href}
-              className="text-sm font-medium transition-colors hover:text-primary"
-            >
-              {navLink.label}
-            </Link>
-          )
-        );
-      })}
-    </nav>
-  );
+interface MainNavProps {
+  navLinks: NavLink[];
+  className?: string;
 }
+
+const NavItem: React.FC<{ navLink: NavLink }> = ({ navLink }) => {
+  const router = useRouter();
+
+  const isDropdownActive = (navLink: NavLink) => {
+    return navLink.subNavLinks?.some(
+      (subNavLink: NavLink) =>
+        subNavLink.href && router.pathname.includes(subNavLink.href)
+    );
+  };
+
+  if (navLink.isDropdown) {
+    return (
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="clear"
+            className={cn(
+              "text-sm font-medium text-muted-foreground transition-colors hover:text-primary p-0",
+              {
+                "text-primary": isDropdownActive(navLink),
+              }
+            )}
+          >
+            {navLink.label} <ChevronDownIcon className="ml-1 h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56" align="end" forceMount>
+          <DropdownMenuGroup>
+            {navLink.subNavLinks &&
+              navLink.subNavLinks.map((subNavLink: NavLink) => (
+                <NavItem key={subNavLink.label} navLink={subNavLink} />
+              ))}
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    );
+  } else if (navLink.isExternal) {
+    return (
+      <a
+        href={navLink.href}
+        key={navLink.label}
+        className="text-sm font-medium transition-colors hover:text-primary"
+      >
+        {navLink.label}
+      </a>
+    );
+  } else {
+    return (
+      <Link
+        href={navLink.href || ""}
+        key={navLink.label}
+        className={cn(
+          "text-sm font-medium text-muted-foreground transition-colors hover:text-primary",
+          {
+            "text-primary": router.pathname === navLink.href,
+          }
+        )}
+      >
+        {navLink.label}
+      </Link>
+    );
+  }
+};
+
+const MainNav = ({ navLinks, className }: MainNavProps) => (
+  <nav className={cn("flex items-center space-x-4 lg:space-x-6", className)}>
+    {navLinks.map((navLink) => (
+      <NavItem key={navLink.label} navLink={navLink} />
+    ))}
+  </nav>
+);
+
+export default MainNav;
