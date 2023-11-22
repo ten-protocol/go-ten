b945b10c7553ebe65593fe607c010c6065015c85
otherview <otherview@gmail.com>
2023-11-22 16:35:11 +0000

    Adding Tx Hash to the faucet funding

--- tools/faucet/faucet/faucet.go
+++ tools/faucet/faucet/faucet.go
@@ -51,34 +51,34 @@ func NewFaucet(rpcURL string, chainID int64, pkString string) (*Faucet, error) {
 	}, nil
 }
 
-func (f *Faucet) Fund(address *common.Address, token string, amount *big.Int) error {
+func (f *Faucet) Fund(address *common.Address, token string, amount *big.Int) (string, error) {
 	var err error
 	var signedTx *types.Transaction
 
 	if token == NativeToken || token == DeprecatedNativeToken {
 		signedTx, err = f.fundNativeToken(address, amount)
 	} else {
-		return fmt.Errorf("token not fundable atm")
+		return "", fmt.Errorf("token not fundable atm")
 		// signedTx, err = f.fundERC20Token(address, token)
 		// todo implement this when contracts are deployable somewhere
 	}
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	// the faucet should be the only user of the faucet pk
 	txMarshal, err := json.Marshal(signedTx)
 	if err != nil {
-		return err
+		return "", err
 	}
 	f.Logger.Info(fmt.Sprintf("Funded address: %s - tx: %+v\n", address.Hex(), string(txMarshal)))
 	// todo handle tx receipt
 
 	if err := f.validateTx(signedTx); err != nil {
-		return fmt.Errorf("unable to validate tx %s: %w", signedTx.Hash(), err)
+		return "", fmt.Errorf("unable to validate tx %s: %w", signedTx.Hash(), err)
 	}
 
-	return nil
+	return signedTx.Hash().Hex(), nil
 }
 
 func (f *Faucet) validateTx(tx *types.Transaction) error {
