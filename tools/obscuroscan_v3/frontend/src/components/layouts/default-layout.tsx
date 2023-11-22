1354cfcdf06cc0a4733711065ef03826aee4fa0f
Jennie <echenimjennifer@gmail.com>
2023-11-22 14:55:39 +0400

    style: update custom font, logo - default layout uniformity

--- tools/obscuroscan_v3/frontend/src/components/layouts/default-layout.tsx
+++ tools/obscuroscan_v3/frontend/src/components/layouts/default-layout.tsx
@@ -8,8 +8,8 @@ interface LayoutProps {
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="flex-col md:flex px-6">
-      <div className="flex flex-col min-h-screen max-w-[1400px] mx-auto">
+    <div className="bg-background">
+      <div className="flex flex-col min-h-screen max-w-[1400px] mx-auto px-4">
         <Header />
         <div className="flex-1 space-y-4 py-6">{children}</div>
         <Footer />
--- tools/obscuroscan_v3/frontend/tailwind.config.ts
+++ tools/obscuroscan_v3/frontend/tailwind.config.ts
@@ -66,8 +66,19 @@ const config: Config = {
         md: `calc(var(--radius) - 2px)`,
         sm: "calc(var(--radius) - 4px)",
       },
-      fontFamily: {
-        cloud: ["Cloud Soft", ...fontFamily.sans],
+      fontSize: {
+        8: "8px",
+        10: "10px",
+        12: "12px",
+        13: "13px",
+        14: "14px",
+        16: "16px",
+        18: "18px",
+        20: "20px",
+        22: "22px",
+        24: "24px",
+        30: "30px",
+        40: "40px",
       },
       keyframes: {
         "accordion-down": {
