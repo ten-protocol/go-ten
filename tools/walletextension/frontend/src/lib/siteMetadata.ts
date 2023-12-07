820ce4e4186fe68a11e7b8de37531a95f30b9292
Jennifer Echenim <echenimjennifer@gmail.com>
2023-12-07 16:05:52 +0400

    update meta description

--- tools/walletextension/frontend/src/lib/siteMetadata.ts
+++ tools/walletextension/frontend/src/lib/siteMetadata.ts
@@ -5,7 +5,7 @@ export const siteMetadata = {
   metaTitle:
     "Ten, a decentralized Layer 2 Rollup protocol designed to hyper-scale and encrypt the Ethereum blockchain.",
   description:
-    "Ten, a decentralized Ethereum Layer 2 Rollup protocol designed to hyper-scale, encrypt and prevent negative MEV on the Ethereum blockchain using Secure Enclaves and ZKPs.",
+    "Ten, a decentralized Ethereum Layer 2 Rollup protocol designed to hyper-scale, encrypt and prevent negative MEV on Ethereum.",
   siteUrl: "https://obscu.ro",
   siteLogo: ``,
   siteLogoSquare: ``,
