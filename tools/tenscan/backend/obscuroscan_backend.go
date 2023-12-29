ba87900a58ba9629f052644676050ad660a5a791
Jennifer Echenim <echenimjennifer@gmail.com>
2023-12-29 21:34:26 +0400

    Refactor health status method names

--- tools/tenscan/backend/obscuroscan_backend.go
+++ tools/tenscan/backend/obscuroscan_backend.go
@@ -30,8 +30,8 @@ func (b *Backend) GetLatestBatch() (*common.BatchHeader, error) {
 	return b.obsClient.BatchHeaderByNumber(nil)
 }
 
-func (b *Backend) GetHealthStatus() (bool, error) {
-	return b.obsClient.HealthStatusOfNode()
+func (b *Backend) GetTenNodeHealthStatus() (bool, error) {
+	return b.obsClient.Health()
 }
 
 func (b *Backend) GetLatestRollup() (*common.RollupHeader, error) {
