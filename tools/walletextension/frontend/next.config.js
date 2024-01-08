6094dc116448f3410ef4318e685953294fb574d3
Jennifer Echenim <echenimjennifer@gmail.com>
2024-01-08 17:22:28 +0400

    Update distDir in next.config.js

--- tools/walletextension/frontend/next.config.js
+++ tools/walletextension/frontend/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   output: "export",
   // distDir should be "../api/static" in production but .next in development
-  distDir: process.env.NODE_ENV === "production" ? "../api/static" : ".next",
+  distDir: process.env.NODE_ENV === "development" ? ".next" : "../api/static",
 };
 
 module.exports = nextConfig;
