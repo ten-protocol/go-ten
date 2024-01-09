5aee83a8fb681890cb45876a664d610d8a3b0496
Jennifer Echenim <echenimjennifer@gmail.com>
2024-01-09 16:47:41 +0400

    [Gateway] health check on gateway UI/frontend (#1729)
    * feat: format health response
    
    * Update getHealthStatus API route
    
    * fix useEffect warning msgs

---
+++ tools/walletextension/frontend/src/components/health-indicator.tsx
@@ -0,0 +1,57 @@
+import React, { useEffect } from "react";
+import { Badge, badgeVariants } from "./ui/badge";
+import useGatewayService from "@/services/useGatewayService";
+import { Skeleton } from "./ui/skeleton";
+
+const HealthIndicator = () => {
+  const { getTestnetStatus } = useGatewayService();
+  const [loading, setLoading] = React.useState(false);
+  const [status, setStatus] = React.useState<boolean>();
+
+  const testnetStatus = async () => {
+    setLoading(true);
+    try {
+      const status = await getTestnetStatus();
+      return status;
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // using useRef to avoid memory leak
+  let isMounted = React.useRef(true);
+
+  useEffect(() => {
+    testnetStatus().then((res) => {
+      if (isMounted.current) {
+        setStatus(res?.result);
+      }
+    });
+
+    return () => {
+      isMounted.current = false;
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div className="flex items-center space-x-2 border rounded-lg p-2">
+      <h3 className="text-sm">Testnet Status: </h3>
+      {loading ? (
+        <Skeleton className="h-4 w-10" />
+      ) : (
+        <Badge
+          variant={
+            (status ? "success" : "destructive") as keyof typeof badgeVariants
+          }
+        >
+          {status ? "Live" : "Down"}
+        </Badge>
+      )}
+    </div>
+  );
+};
+
+export default HealthIndicator;
