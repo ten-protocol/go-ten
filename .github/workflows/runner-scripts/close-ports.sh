40030c7d0110da3a9e3e0323c3f87f1ad2e20be2
Anthony Nixon <anthony@obscu.ro>
2024-02-14 16:46:30 +0000

    test echos

--- .github/workflows/runner-scripts/close-ports.sh
+++ .github/workflows/runner-scripts/close-ports.sh
@@ -3,11 +3,16 @@
 # Ensure any fail is loud
 set -euo pipefail
 
+echo "start script and set ports vars"
+
 lowest_port=8000
 highest_port=58000
 additional_ports=(80 81 99)
 
+echo "fetch pids"
 pids=$(lsof -iTCP:$lowest_port-$highest_port -sTCP:LISTEN -t)
+
+echo "list pids $pids and process range first"
 if [ -z "$pids" ]; then
   echo "No processes are listening on ports from $lowest_port to $highest_port"
 else
@@ -16,10 +21,10 @@ else
   done
   for pid in $pids; do
     echo "Killing process $pid on one of the ports from $lowest_port to $highest_port"
-    kill -9 $pid || true
+    kill $pid || true
   done
 fi
-
+echo "range done"
 echo "Additional ports: ${additional_ports[@]}"
 for port in "${additional_ports[@]}"; do
   pids=$(lsof -ti TCP:$port)
