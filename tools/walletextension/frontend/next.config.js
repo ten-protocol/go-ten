1782b6231d8d7601a321fd37e2281d783ad10c39
Jennifer Echenim <echenimjennifer@gmail.com>
2024-02-19 14:52:37 +0100

    [TG] update Ten's visual identity (#1796)
    * update Ten logo
    
    * - add new fonts, favicon and update global styles

--- tools/walletextension/frontend/next.config.js
+++ tools/walletextension/frontend/next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   output: "export",
   // distDir should be "../api/static" in production but .next in development
   distDir: process.env.NODE_ENV === "development" ? ".next" : "../api/static",
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
