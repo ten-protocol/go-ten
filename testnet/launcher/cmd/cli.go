82ae7a6ccd5be897acbf9892e39f0dda450e5415
Anthony Nixon <anthony@obscu.ro>
2024-03-01 12:24:56 +0000

    Testnet to allow .env, environment vars, or flags for config.

--- testnet/launcher/cmd/cli.go
+++ testnet/launcher/cmd/cli.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"flag"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 )
 
 // TestnetConfigCLI represents the configurations passed into the testnet over CLI
@@ -21,14 +25,14 @@ func ParseConfigCLI() *TestnetConfigCLI {
 	cfg := &TestnetConfigCLI{}
 	flagUsageMap := getFlagUsageMap()
 
-	validatorEnclaveDockerImage := flag.String(validatorEnclaveDockerImageFlag, "testnetobscuronet.azurecr.io/obscuronet/enclave:latest", flagUsageMap[validatorEnclaveDockerImageFlag])
-	validatorEnclaveDebug := flag.Bool(validatorEnclaveDebugFlag, false, flagUsageMap[validatorEnclaveDebugFlag])
-	sequencerEnclaveDockerImage := flag.String(sequencerEnclaveDockerImageFlag, "testnetobscuronet.azurecr.io/obscuronet/enclave:latest", flagUsageMap[sequencerEnclaveDockerImageFlag])
-	sequencerEnclaveDebug := flag.Bool(sequencerEnclaveDebugFlag, false, flagUsageMap[sequencerEnclaveDebugFlag])
-	contractDeployerDockerImage := flag.String(contractDeployerDockerImageFlag, "testnetobscuronet.azurecr.io/obscuronet/hardhatdeployer:latest", flagUsageMap[contractDeployerDockerImageFlag])
-	contractDeployerDebug := flag.Bool(contractDeployerDebugFlag, false, flagUsageMap[contractDeployerDebugFlag])
-	isSGXEnabled := flag.Bool(isSGXEnabledFlag, false, flagUsageMap[isSGXEnabledFlag])
-	logLevel := flag.Int(logLevelFlag, 4, flagUsageMap[logLevelFlag])
+	validatorEnclaveDockerImage := flag.String(validatorEnclaveDockerImageFlag, getEnvOrDefault(validatorEnclaveDockerImageFlag, "testnetobscuronet.azurecr.io/obscuronet/enclave:latest"), flagUsageMap[validatorEnclaveDockerImageFlag])
+	validatorEnclaveDebug := flag.Bool(validatorEnclaveDebugFlag, getEnvOrDefaultBool(validatorEnclaveDebugFlag, false), flagUsageMap[validatorEnclaveDebugFlag])
+	sequencerEnclaveDockerImage := flag.String(sequencerEnclaveDockerImageFlag, getEnvOrDefault(sequencerEnclaveDockerImageFlag, "testnetobscuronet.azurecr.io/obscuronet/enclave:latest"), flagUsageMap[sequencerEnclaveDockerImageFlag])
+	sequencerEnclaveDebug := flag.Bool(sequencerEnclaveDebugFlag, getEnvOrDefaultBool(validatorEnclaveDebugFlag, false), flagUsageMap[sequencerEnclaveDebugFlag])
+	contractDeployerDockerImage := flag.String(contractDeployerDockerImageFlag, getEnvOrDefault(contractDeployerDockerImageFlag, "testnetobscuronet.azurecr.io/obscuronet/hardhatdeployer:latest"), flagUsageMap[contractDeployerDockerImageFlag])
+	contractDeployerDebug := flag.Bool(contractDeployerDebugFlag, getEnvOrDefaultBool(validatorEnclaveDebugFlag, false), flagUsageMap[contractDeployerDebugFlag])
+	isSGXEnabled := flag.Bool(isSGXEnabledFlag, getEnvOrDefaultBool(validatorEnclaveDebugFlag, false), flagUsageMap[isSGXEnabledFlag])
+	logLevel := flag.Int(logLevelFlag, getEnvOrDefaultInt(logLevelFlag, 4), flagUsageMap[logLevelFlag])
 	flag.Parse()
 
 	cfg.validatorEnclaveDockerImage = *validatorEnclaveDockerImage
@@ -41,4 +45,43 @@ func ParseConfigCLI() *TestnetConfigCLI {
 	cfg.logLevel = *logLevel
 
 	return cfg
+}
+
+// getEnvOrDefault tries to get an associated environment variable; if not present, returns a default value.
+func getEnvOrDefault(flagName string, defaultValue string) string {
+	envName := strings.ToUpper(strings.ReplaceAll(flagName, "-", "_"))
+	if value, exists := os.LookupEnv(envName); exists {
+		return value
+	}
+	return defaultValue
+}
+
+// getEnvOrDefaultBool tries to get an associated environment variable as boolean; if not present, returns a default value.
+func getEnvOrDefaultBool(flagName string, defaultValue bool) bool {
+	envName := strings.ToUpper(strings.ReplaceAll(flagName, "-", "_"))
+	if value, exists := os.LookupEnv(envName); exists {
+		boolValue, err := strconv.ParseBool(value)
+		if err != nil {
+			// Log the error
+			log.Printf("Error parsing environment variable %s=%s as boolean: %v", envName, value, err)
+			return defaultValue
+		}
+		return boolValue
+	}
+	return defaultValue
+}
+
+// getEnvOrDefault tries to get an associated environment variable as integer; if not present, returns a default value.
+func getEnvOrDefaultInt(flagName string, defaultValue int) int {
+	envName := strings.ToUpper(strings.ReplaceAll(flagName, "-", "_"))
+	if value, exists := os.LookupEnv(envName); exists {
+		intValue, err := strconv.Atoi(value)
+		if err != nil {
+			// Log the error
+			log.Printf("Error parsing environment variable %s=%s as integer: %v", envName, value, err)
+			return defaultValue
+		}
+		return intValue
+	}
+	return defaultValue
 }
--- testnet/launcher/cmd/main.go
+++ testnet/launcher/cmd/main.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"fmt"
+	"github.com/joho/godotenv"
+	"log"
 	"os"
 
 	"github.com/ten-protocol/go-ten/testnet/launcher"
 )
 
 func main() {
+	// Try to overload environment variables if .env file exists
+	errEnv := godotenv.Overload()
+	if errEnv != nil {
+		log.Printf("Optional .env file not supplied: %v", errEnv)
+	}
+
 	cliConfig := ParseConfigCLI()
 
 	fmt.Println("Starting a testnet with 1 sequencer and 1 validator...")
