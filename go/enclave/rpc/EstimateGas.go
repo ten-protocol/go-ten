014cb54df6dc1cfebbbbe40a30160463d1e54d84
Tudor Malene <tudor.malene@gmail.com>
2024-03-11 19:42:00 +0000

    fix

--- go/enclave/rpc/EstimateGas.go
+++ go/enclave/rpc/EstimateGas.go
@@ -53,11 +53,11 @@ func EstimateGasValidate(reqParams []any, builder *CallBuilder[CallParamsWithBlo
 }
 
 func EstimateGasExecute(builder *CallBuilder[CallParamsWithBlock, hexutil.Uint64], rpc *EncryptionManager) error {
-	err := authenticateFrom(builder.VK, builder.From)
-	if err != nil {
-		builder.Err = err
-		return nil //nolint:nilerr
-	}
+	//err := authenticateFrom(builder.VK, builder.From)
+	//if err != nil {
+	//	builder.Err = err
+	//	return nil //nolint:nilerr
+	//}
 
 	txArgs := builder.Param.callParams
 	blockNumber := builder.Param.block
