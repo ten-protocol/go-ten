2325b7d3af4d3971b2f32bda5aa71b6c3dfd3c52
Matt Curtis <matt@obscu.ro>
2024-03-18 09:35:34 +0000

    Remove debugging from contract

--- contracts/src/management/ManagementContract.sol
+++ contracts/src/management/ManagementContract.sol
@@ -82,22 +82,10 @@ contract ManagementContract is Initializable, OwnableUpgradeable {
         // todo: verify this enclaveID is a permissioned Sequencer enclaveID
         address enclaveID = ECDSA.recover(r.Hash, r.Signature);
         // revert if the EnclaveID is not attested
-        require(attested[enclaveID], string(abi.encodePacked("enclaveID not attested: ", addressToString(enclaveID))));
+        require(attested[enclaveID], "enclaveID not attested");
 
         AppendRollup(r);
         pushCrossChainMessages(crossChainData);
-    }
-    function addressToString(address _addr) private pure returns (string memory) {
-        bytes32 value = bytes32(uint256(uint160(_addr)));
-        bytes memory alphabet = "0123456789abcdef";
-        bytes memory str = new bytes(42);
-        str[0] = '0';
-        str[1] = 'x';
-        for (uint256 i = 0; i < 20; i++) {
-            str[2+i*2] = alphabet[uint8(value[i + 12] >> 4)];
-            str[3+i*2] = alphabet[uint8(value[i + 12] & 0x0f)];
-        }
-        return string(str);
     }
 
     // InitializeNetworkSecret kickstarts the network secret, can only be called once
