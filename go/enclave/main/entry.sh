f7fef30394b981859cd4563aa1f2451ed91627c4
Tudor Malene <tudor.malene@gmail.com>
2024-04-10 09:47:48 +0100

    update pccs url (#1871)

--- go/enclave/main/entry.sh
+++ go/enclave/main/entry.sh
@@ -11,21 +11,11 @@ if [ ! -L /dev/sgx/enclave ]; then
 	ln -s /dev/sgx_enclave /dev/sgx/enclave
 fi
 
-# If the PCCS_ADDR, the host is provided
-# Do not use the default PCCS_URL defined in /etc/sgx_default_qcnl.conf
-# Particularly used in Alibaba cloud
-if [ -n "${PCCS_ADDR}" ]; then
-	PCCS_URL=https://${PCCS_ADDR}/sgx/certification/v3/
-fi
+PCCS_URL=https://global.acccache.azure.net/sgx/certification/v4/
+echo "PCCS_URL: ${PCCS_URL}"
 
-# Install the libsgx-dcap-default-qpl and redefine /etc/sgx_default_qcnl.conf (Alibaba)
-if [ -n "${PCCS_URL}" ]; then
-	apt-get install -qq libsgx-dcap-default-qpl
-	echo "PCCS_URL: ${PCCS_URL}"
-	echo "PCCS_URL=${PCCS_URL}\nUSE_SECURE_CERT=FALSE" > /etc/sgx_default_qcnl.conf
-else
-# Otherwise use the Azure library
-	apt-get install -qq az-dcap-client
-fi
+apt-get install -qq libsgx-dcap-default-qpl
+
+echo "PCCS_URL=${PCCS_URL}\nUSE_SECURE_CERT=FALSE" > /etc/sgx_default_qcnl.conf
 
 "$@"
