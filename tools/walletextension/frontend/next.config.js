7d4c7381133eff2fa05b51ce3000b672103408d8
Žiga Kokelj <ziga.kokelj@gmail.com>
2024-05-13 15:44:03 +0200

    revert the changes for next.config.js to avoid issues with the next release

--- tools/walletextension/frontend/next.config.js
+++ tools/walletextension/frontend/next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  // distDir should be "../api/static" in production but .next in development
+  distDir: process.env.NODE_ENV === "development" ? ".next" : "../api/static",
   images: {
     unoptimized: true,
   },
+  // base path for static files should be "" in development but "/static" in production
+  basePath: process.env.NODE_ENV === "development" ? "" : "/static",
 };
 
 module.exports = nextConfig;
