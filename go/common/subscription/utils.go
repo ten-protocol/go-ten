b880d5b1f5e6ce8a3068b65e73bb5f6865dfe987
Tudor Malene <tudor.malene@gmail.com>
2024-05-15 10:52:26 +0100

    close ticker

--- go/common/subscription/utils.go
+++ go/common/subscription/utils.go
@@ -17,10 +17,12 @@ import (
 func ForwardFromChannels[R any](inputChannels []chan R, onMessage func(R) error, onBackendDisconnect func(), backendDisconnected *atomic.Bool, stopped *atomic.Bool, timeoutInterval time.Duration, logger gethlog.Logger) {
 	inputCases := make([]reflect.SelectCase, len(inputChannels)+1)
 
+	cleanupTicker := time.NewTicker(2 * time.Second)
+	defer cleanupTicker.Stop()
 	// create a ticker to handle cleanup, check the "stopped" flag and exit the goroutine
 	inputCases[0] = reflect.SelectCase{
 		Dir:  reflect.SelectRecv,
-		Chan: reflect.ValueOf(time.NewTicker(2 * time.Second).C),
+		Chan: reflect.ValueOf(cleanupTicker.C),
 	}
 
 	// create a select "case" for each input channel
