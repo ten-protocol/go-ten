dc1ee211ede79cee834e6a089b045dcf36ef2e48
Žiga Kokelj <ziga.kokelj@gmail.com>
2024-05-16 09:46:24 +0200

    remove redirections as they will be handled on Azure for the deployed gateway

--- tools/walletextension/frontend/next.config.js
+++ tools/walletextension/frontend/next.config.js
@@ -4,23 +4,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  async redirects() {
-    const destinationUrl = process.env.NEXT_PUBLIC_API_GATEWAY_URL || 'https://testnet.ten.xyz';
-    return [
-      {
-        source: '/v1/:path*',
-        destination: `${destinationUrl}/v1/:path*`,
-        permanent: true,
-        has: [
-          {
-            type: 'header',
-            key: 'x-forwarded-proto',
-            value: '(https|http)',
-          },
-        ],
-      },
-    ]
-  },
 };
 
 module.exports = nextConfig;
