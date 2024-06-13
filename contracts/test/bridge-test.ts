b537d2e6a8393fc1ee20201803c52d33d2f1d3c9
Stefan Iliev <46542846+StefanIliev545@users.noreply.github.com>
2024-06-13 13:32:06 +0300

    Add indexed events (#1954)
    * Add indexing for bridge events.
    
    * Fixes for hardhat tests.
    
    * Fix for processing of events.
    
    ---------
    
    Co-authored-by: StefanIliev545 <stefan@obscu.ro>

--- contracts/test/bridge-test.ts
+++ contracts/test/bridge-test.ts
@@ -79,7 +79,7 @@ describe("Bridge", function () {
 
       const eventSignature = "LogMessagePublished(address,uint64,uint32,uint32,bytes,uint8)";
       const topic = ethers.id(eventSignature)
-      let eventIface = new ethers.Interface([ `event ${eventSignature}`]);
+      let eventIface = new ethers.Interface([ `event LogMessagePublished(address indexed,uint64,uint32,uint32,bytes,uint8)`]);
 
       const receipt = (await  tx.wait())!!;
 
@@ -93,10 +93,10 @@ describe("Bridge", function () {
      
       const promises = events.map(async (event) => {
           const decodedEvent = eventIface.parseLog({
-            topics: event!.topics!.map((v)=>v),
+            topics: event!.topics!,
             data: event!.data
           })!!;
-    
+              
           const xchainMessage = {
             sender: decodedEvent.args[0],
             sequence: decodedEvent.args[1],
@@ -283,7 +283,7 @@ describe("Bridge", function () {
 
       await expect(event).to.not.be.undefined;
 
-      let eventIface = new ethers.Interface([ `event ${eventSignature}`]);
+      let eventIface = new ethers.Interface([ `event LogMessagePublished(address indexed,uint64,uint32,uint32,bytes,uint8)`]);
 
       const decodedEvent = eventIface.parseLog({
         topics: event!.topics!.map((v)=>v),
