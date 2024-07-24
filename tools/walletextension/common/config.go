ad61bdb04fcdfdeb6013bd50b1eb3d63315828ca
Žiga Kokelj <ziga.kokelj@gmail.com>
2024-07-24 14:16:15 +0200

    Gateway rate limiting (#1942)

--- tools/walletextension/common/config.go
+++ tools/walletextension/common/config.go
@@ -1,17 +1,22 @@
 package common
 
+import "time"
+
 // Config contains the configuration required by the WalletExtension.
 type Config struct {
-	WalletExtensionHost     string
-	WalletExtensionPortHTTP int
-	WalletExtensionPortWS   int
-	NodeRPCHTTPAddress      string
-	NodeRPCWebsocketAddress string
-	LogPath                 string
-	DBPathOverride          string // Overrides the database file location. Used in tests.
-	VerboseFlag             bool
-	DBType                  string
-	DBConnectionURL         string
-	TenChainID              int
-	StoreIncomingTxs        bool
+	WalletExtensionHost            string
+	WalletExtensionPortHTTP        int
+	WalletExtensionPortWS          int
+	NodeRPCHTTPAddress             string
+	NodeRPCWebsocketAddress        string
+	LogPath                        string
+	DBPathOverride                 string // Overrides the database file location. Used in tests.
+	VerboseFlag                    bool
+	DBType                         string
+	DBConnectionURL                string
+	TenChainID                     int
+	StoreIncomingTxs               bool
+	RateLimitUserComputeTime       time.Duration
+	RateLimitWindow                time.Duration
+	RateLimitMaxConcurrentRequests int
 }
