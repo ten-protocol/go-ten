ff52e8f8cac47d003e7e93e4c1d78a824002f2f6
Will Hester <william_hester@me.com>
2024-07-30 14:09:23 +0100

    add legeacy peer deps to hh build

--- testnet/launcher/eth2network/docker.go
+++ testnet/launcher/eth2network/docker.go
@@ -44,26 +44,6 @@ func (n *Eth2Network) Start() error {
 		exposedPorts = append(exposedPorts, n.cfg.gethWSPort)
 	}
 
-	if n.cfg.gethNetworkPort != 0 {
-		cmds = append(cmds, "-gethNetworkStartPort", fmt.Sprintf("%d", n.cfg.gethNetworkPort))
-		exposedPorts = append(exposedPorts, n.cfg.gethNetworkPort)
-	}
-
-	if n.cfg.gethRPCPort != 0 {
-		cmds = append(cmds, "-gethAuthRPCStartPort", fmt.Sprintf("%d", n.cfg.gethRPCPort))
-		exposedPorts = append(exposedPorts, n.cfg.gethRPCPort)
-	}
-
-	if n.cfg.prysmP2PPort != 0 {
-		cmds = append(cmds, "-prysmBeaconP2PStartPort", fmt.Sprintf("%d", n.cfg.prysmP2PPort))
-		exposedPorts = append(exposedPorts, n.cfg.prysmP2PPort)
-	}
-
-	if n.cfg.prysmRPCPort != 0 {
-		cmds = append(cmds, "-prysmBeaconRPCStartPort", fmt.Sprintf("%d", n.cfg.prysmRPCPort))
-		exposedPorts = append(exposedPorts, n.cfg.prysmRPCPort)
-	}
-
 	// keep a volume of binaries to avoid downloading
 	volume := map[string]string{"eth2_bin": "/home/obscuro/go-obscuro/integration/.build/eth2_bin/"}
 
