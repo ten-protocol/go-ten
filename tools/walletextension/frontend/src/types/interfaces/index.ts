7dc9a68857a00fcfaf42c63e20963a43107ce72a
Jennifer Echenim <echenimjennifer@gmail.com>
2024-09-30 20:05:52 +0400

    wip: gateway wallet store/service

--- tools/walletextension/frontend/src/types/interfaces/index.ts
+++ tools/walletextension/frontend/src/types/interfaces/index.ts
@@ -1,4 +1,5 @@
-import React from "react";
+import { IWalletState } from "@repo/ui/lib/interfaces/wallet";
+import { Account } from "./WalletInterfaces";
 
 export type NavLink = {
   label: string;
@@ -7,3 +8,25 @@ export type NavLink = {
   isExternal?: boolean;
   subNavLinks?: NavLink[];
 };
+
+export type StoreSet = (
+  partial:
+    | IGatewayWalletState
+    | Partial<IGatewayWalletState>
+    | ((
+        state: IGatewayWalletState
+      ) => IGatewayWalletState | Partial<IGatewayWalletState>),
+  replace?: boolean | undefined
+) => void;
+
+export type StoreGet = () => IGatewayWalletState;
+
+interface IGatewayWalletState extends IWalletState {
+  token: string;
+  version: string | null;
+  accounts: Account[] | null;
+
+  initializeGateway: () => void;
+  connectAccount: (account: string) => void;
+  revokeAccounts: () => void;
+}
