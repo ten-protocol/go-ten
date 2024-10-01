0c26de2ab21052a9dd8abb34a10b138968af0099
Jennifer Echenim <echenimjennifer@gmail.com>
2024-10-01 18:12:03 +0400

    Revert "[Frontend] monorepo config"

--- packages/ui/lib/enums/error.ts
+++
@@ -1,9 +0,0 @@
-export enum IErrorMessages {
-  UnknownAccount = "unknown account",
-  InsufficientFunds = "insufficient funds",
-  UserDeniedTransactionSignature = "User denied transaction signature",
-  UserRejectedTheRequest = "User rejected the request",
-  ExecutionReverted = "execution reverted",
-  RateLimitExceeded = "rate limit exceeded",
-  WithdrawalSpent = "withdrawal already spent",
-}
