ea83312de4712946e33ec0a650745c393855a0b1
Tudor Malene <tudor.malene@gmail.com>
2024-10-28 11:51:52 +0000

    add leak detection

--- go/enclave/main/main.go
+++ go/enclave/main/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"runtime/pprof"
+	"time"
 
 	"github.com/ten-protocol/go-ten/go/common/container"
 	tenflag "github.com/ten-protocol/go-ten/go/common/flag"
@@ -24,6 +27,29 @@ func main() {
 	if err != nil {
 		panic(fmt.Errorf("unable to create config from flags - %w", err))
 	}
+
+	// temporary code to help identify OOM
+	go func() {
+		for {
+			heap, err := os.Create(fmt.Sprintf("/data/heap_%d.pprof", time.Now().UnixMilli()))
+			if err != nil {
+				panic(fmt.Errorf("could not open heap profile: %w", err))
+			}
+			err = pprof.WriteHeapProfile(heap)
+			if err != nil {
+				panic(fmt.Errorf("could not write CPU profile: %w", err))
+			}
+			stack, err := os.Create(fmt.Sprintf("/data/stack_%d.pprof", time.Now().UnixMilli()))
+			if err != nil {
+				panic(fmt.Errorf("could not open stack profile: %w", err))
+			}
+			err = pprof.Lookup("goroutine").WriteTo(stack, 1)
+			if err != nil {
+				panic(fmt.Errorf("could not write CPU profile: %w", err))
+			}
+			time.Sleep(1 * time.Hour)
+		}
+	}()
 
 	enclaveContainer := enclavecontainer.NewEnclaveContainerFromConfig(enclaveConfig)
 	container.Serve(enclaveContainer)
--- go/host/main/main.go
+++ go/host/main/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"runtime/pprof"
+	"time"
 
 	"github.com/ten-protocol/go-ten/go/common/container"
 	hostcontainer "github.com/ten-protocol/go-ten/go/host/container"
@@ -13,6 +16,29 @@ func main() {
 	if err != nil {
 		panic(fmt.Errorf("could not parse config. Cause: %w", err))
 	}
+
+	// temporary code to help identify OOM
+	go func() {
+		for {
+			heap, err := os.Create(fmt.Sprintf("/data/heap_%d.pprof", time.Now().UnixMilli()))
+			if err != nil {
+				panic(fmt.Errorf("could not open heap profile: %w", err))
+			}
+			err = pprof.WriteHeapProfile(heap)
+			if err != nil {
+				panic(fmt.Errorf("could not write CPU profile: %w", err))
+			}
+			stack, err := os.Create(fmt.Sprintf("/data/stack_%d.pprof", time.Now().UnixMilli()))
+			if err != nil {
+				panic(fmt.Errorf("could not open stack profile: %w", err))
+			}
+			err = pprof.Lookup("goroutine").WriteTo(stack, 1)
+			if err != nil {
+				panic(fmt.Errorf("could not write CPU profile: %w", err))
+			}
+			time.Sleep(1 * time.Hour)
+		}
+	}()
 
 	hostContainer := hostcontainer.NewHostContainerFromConfig(parsedConfig, nil)
 	container.Serve(hostContainer)
