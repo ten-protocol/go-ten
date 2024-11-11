5a9dd8120cdfe125dee914df41b71023a36d6abb
Will Hester <william_hester@me.com>
2024-11-11 16:56:05 +0000

    remove ID from identity

--- go/common/host/identity.go
+++ go/common/host/identity.go
@@ -1,13 +1,11 @@
 package host
 
 import (
-	gethcommon "github.com/ethereum/go-ethereum/common"
 	"github.com/ten-protocol/go-ten/go/common"
 	hostconfig "github.com/ten-protocol/go-ten/go/host/config"
 )
 
 type Identity struct {
-	ID               gethcommon.Address
 	P2PPublicAddress string
 	IsGenesis        bool
 	IsSequencer      bool
@@ -15,7 +13,6 @@ type Identity struct {
 
 func NewIdentity(cfg *hostconfig.HostConfig) Identity {
 	return Identity{
-		ID:               cfg.ID,
 		P2PPublicAddress: cfg.P2PPublicAddress,
 		IsGenesis:        cfg.IsGenesis,
 		IsSequencer:      cfg.NodeType == common.Sequencer,
