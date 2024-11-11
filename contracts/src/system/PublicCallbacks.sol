b70a9ec5beffa99fda036d1970b2824c3ef1d0ad
StefanIliev545 <stefan@obscu.ro>
2024-11-11 17:17:17 +0200

    Upgraded contract.

--- contracts/src/system/PublicCallbacks.sol
+++ contracts/src/system/PublicCallbacks.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.28;
 
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
@@ -17,7 +17,7 @@ contract PublicCallbacks is Initializable {
     }
 
     struct Callback {
-        address callback;
+        address target;
         bytes data;
         uint256 value;
     }
@@ -32,13 +32,25 @@ contract PublicCallbacks is Initializable {
     }
 
     function addCallback(address callback, bytes calldata data, uint256 value) internal {
-        callbacks[nextCallbackId++] = Callback({callback: callback, data: data, value: value});
+        callbacks[nextCallbackId++] = Callback({target: callback, data: data, value: value});
     }
 
     function register(bytes calldata callback) external payable { 
         addCallback(msg.sender, callback, msg.value);
     }
 
+    // reattempt a callback that failed to execute.
+    // This is callable from external users and fully passes over the gas given to this call.
+    function reattemptCallback(uint256 callbackId) external {
+        Callback memory callback = callbacks[callbackId];
+        (bool success, ) = callback.target.call(callback.data);
+        require(success, "Callback execution failed");
+        // nothing to refund; the callback was already paid for during its failure
+    }
+
+    // System level call. As it is called during a synthetic transaction that does not have gas limit, 
+    // the contract enforces a custom limit based on the value stored for the callback.
+    // It attempts to somewhat accurately refund.
     function executeNextCallback() external onlySelf {
         if (nextCallbackId > lastUnusedCallbackId) {
             return; // todo: change to revert if possible
@@ -46,10 +58,32 @@ contract PublicCallbacks is Initializable {
 
         uint256 callbackId = lastUnusedCallbackId++;
         require(callbackId < lastUnusedCallbackId, "Paranoia- todo: delete");
-        Callback memory callback = callbacks[callbackId];
-        (bool success, ) = callback.callback.call{value: callback.value}(callback.data);
+        Callback storage callback = callbacks[callbackId];
+        uint256 baseFee = block.basefee;
+        uint256 gas = callback.value / baseFee;
+        uint256 gasBefore = gasleft();
+        (bool success, ) = callback.target.call{gas: gas}(callback.data);
         if (success) {
             delete callbacks[callbackId];
         }
+        uint256 gasAfter = gasleft();
+        uint256 gasRefund = (gasBefore - gasAfter) * baseFee;
+        internalRefund(gasRefund, callback.target);
+        callback.value = callback.value - gasRefund;
+        payForCallback(callback.value);
+    }
+
+    function internalRefund(uint256 gasRefund, address to) internal {
+        // 22k is the max refund gas limit; 21k for a call and a bit for any accounting the contract might have.
+        // ordinarily such accounting should be prepared for beforehand in the callback they pay for, but we give them a
+        // slight buffer. 
+        (bool success, ) = to.call{value: gasRefund, gas: 22000}(""); 
+        if (!success) {
+            block.coinbase.transfer(gasRefund); // if they dont accept the refund, we gift it to coinbase.
+        }
+    }
+
+    function payForCallback(uint256 gasPayment) internal {
+        block.coinbase.transfer(gasPayment);
     }
 }
