52cdcdc2524184ee11f8f714957d4b0bac0f1f4c
StefanIliev545 <stefan@obscu.ro>
2024-12-02 15:48:23 +0200

    Addressed PR comments.

--- contracts/src/system/Fees.sol
+++ contracts/src/system/Fees.sol
@@ -13,27 +13,36 @@ contract Fees is Initializable, OwnableUpgradeable {
 
     uint256 private _messageFeePerByte;
 
+    // Constructor disables initializer;
+    // Only owner functions will not be callable on implementation
     constructor() {
         _disableInitializers();
     }
 
+    // initialization function to be used by the proxy.
     function initialize(uint256 initialMessageFeePerByte, address eoaOwner) public initializer {
         __Ownable_init(eoaOwner);
         _messageFeePerByte = initialMessageFeePerByte;
     }
 
+    // Helper function to calculate the fee for a message
     function messageFee(uint256 messageSize) external view returns (uint256) {
         return _messageFeePerByte * messageSize;
     }
 
+    // The EOA owner can set the message fee to ensure sequencer is not publishing
+    // at a loss
     function setMessageFee(uint256 newMessageFeePerByte) external onlyOwner{
         _messageFeePerByte = newMessageFeePerByte;
     }
 
+    // The EOA owner can collect the fees
     function withdrawalCollectedFees() external onlyOwner {
         payable(owner()).transfer(address(this).balance);
     }
 
+    // For now just the whole balance as we only collect fees
+    // from the message bus
     function collectedFees() external view returns (uint256) {
         return address(this).balance;
     }
