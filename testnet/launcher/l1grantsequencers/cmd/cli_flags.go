fec1c4925d55e6c0c7c960b1919e05ea297fb89c
Matt <98158711+BedrockSquirrel@users.noreply.github.com>
2024-12-10 13:57:07 +0000

    Testnets: grant sequencer enclaveID permission on L1 (#2185)

--- testnet/launcher/l1grantsequencers/cmd/cli_flags.go
+++ testnet/launcher/l1grantsequencers/cmd/cli_flags.go
@@ -4,10 +4,11 @@ package main
 const (
 	l1HTTPURLFlag           = "l1_http_url"
 	privateKeyFlag          = "private_key"
-	mgmtContractAddressFlag = "mgmt_contract_address"
+	mgmtContractAddressFlag = "management_contract_addr"
 	enclaveIDsFlag          = "enclave_ids"
 	dockerImageFlag         = "docker_image"
 	contractsEnvFileFlag    = "contracts_env_file"
+	sequencerURLFlag        = "sequencer_url"
 )
 
 // Returns a map of the flag usages.
@@ -17,8 +18,9 @@ func getFlagUsageMap() map[string]string {
 		l1HTTPURLFlag:           "Layer 1 network http RPC addr",
 		privateKeyFlag:          "L1 and L2 private key used in the node",
 		mgmtContractAddressFlag: "L1 management contract address",
-		enclaveIDsFlag:          "List of enclave public keys",
+		enclaveIDsFlag:          "List of enclave public keys to grant sequencer role",
 		dockerImageFlag:         "Docker image to run",
 		contractsEnvFileFlag:    "If set, it will write the contract addresses to the file",
+		sequencerURLFlag:        "Sequencer RPC URL to fetch enclave IDs (required if enclaveIDs are not provided)",
 	}
 }
