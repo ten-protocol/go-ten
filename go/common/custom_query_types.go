9869bd5dbda101a50e9fa3db6901a40f7f28cd4b
Žiga Kokelj <ziga.kokelj@gmail.com>
2024-12-12 11:26:11 +0100

    remove getStorageAt method for returning userID and corresponding test

--- go/common/custom_query_types.go
+++ go/common/custom_query_types.go
@@ -19,7 +19,6 @@ import "github.com/ethereum/go-ethereum/common"
 
 // CustomQuery methods
 const (
-	UserIDRequestCQMethod           = "0x0000000000000000000000000000000000000001"
 	ListPrivateTransactionsCQMethod = "0x0000000000000000000000000000000000000002"
 	CreateSessionKeyCQMethod        = "0x0000000000000000000000000000000000000003"
 	ActivateSessionKeyCQMethod      = "0x0000000000000000000000000000000000000004"
