78f1b86bc90a408096ac607de9080bef68fd16ad
Tudor Malene <tudor.malene@gmail.com>
2025-02-21 09:53:53 +0000

    clarify gas calculation and add cost of L1 tx

--- go/enclave/gas/oracle.go
+++ go/enclave/gas/oracle.go
@@ -11,6 +11,12 @@ import (
 	"github.com/ten-protocol/go-ten/go/common/gethapi"
 )
 
+// L1TxGas - a crude estimation of the cost of publishing an L1 tx
+const L1TxGas = 300_000
+
+// TxsPerBatch - the number of transactions in a batch. A conservative estimation.
+const TxsPerBatch = 50
+
 // Oracle - the interface for the future precompiled gas oracle contract
 // which will expose necessary l1 information.
 type Oracle interface {
@@ -31,7 +37,7 @@ func (o *oracle) EstimateL1StorageGasCost(tx *types.Transaction, block *types.He
 		return nil, err
 	}
 
-	return o.calculateL1Cost(block, encodedTx, header)
+	return o.calculateL1Cost(block, header, encodedTx)
 }
 
 func (o *oracle) EstimateL1CostForMsg(args *gethapi.TransactionArgs, block *types.Header, header *common.BatchHeader) (*big.Int, error) {
@@ -40,25 +46,36 @@ func (o *oracle) EstimateL1CostForMsg(args *gethapi.TransactionArgs, block *type
 		encoded = append(encoded, *args.Data...)
 	}
 
-	return o.calculateL1Cost(block, encoded, header)
+	return o.calculateL1Cost(block, header, encoded)
 }
 
-func (o *oracle) calculateL1Cost(block *types.Header, encodedTx []byte, header *common.BatchHeader) (*big.Int, error) {
-	// If the block does not have excess blob gas, we can't estimate the cost
-	if block.ExcessBlobGas == nil {
-		return big.NewInt(0), nil
+// calculateL1Cost - Calculates the L1 cost as a multiple of the L2 base fee.
+func (o *oracle) calculateL1Cost(l1Block *types.Header, l2Batch *common.BatchHeader, encodedTx []byte) (*big.Int, error) {
+	totalCost := big.NewInt(0)
+
+	// If the l1Block does not have excess blob gas, we can't estimate the cost
+	if l1Block.ExcessBlobGas == nil {
+		return totalCost, nil
 	}
 
-	blobFee := eip4844.CalcBlobFee(*block.ExcessBlobGas)
+	// price in Wei for a single unit of blob
+	blobFeePerByte := eip4844.CalcBlobFee(*l1Block.ExcessBlobGas)
 
-	l1Gas := CalculateL1GasUsed(encodedTx)
-	gasCost := big.NewInt(0).Mul(l1Gas, blobFee)
+	// Calculate teh cost of including the tx in a blob
+	txL1Size := CalculateL1Size(encodedTx)
+	shareOfBlobCost := big.NewInt(0).Mul(txL1Size, blobFeePerByte)
 
-	// round the gasCost up to the nearest multiple of header.BaseFee
-	remainder := new(big.Int).Mod(gasCost, header.BaseFee)
+	// Add a value to the shareOfBlobCost to account for the cost of the transaction itself
+	shareOfL1TxGas := big.NewInt(L1TxGas / TxsPerBatch)
+	shareOfL1TxCost := big.NewInt(0).Mul(shareOfL1TxGas, l1Block.BaseFee)
+
+	totalCost.Add(shareOfBlobCost, shareOfL1TxCost)
+
+	// round the shareOfBlobCost up to the nearest multiple of l2Batch.BaseFee
+	remainder := new(big.Int).Mod(totalCost, l2Batch.BaseFee)
 	if remainder.Sign() > 0 {
-		gasCost.Add(gasCost, new(big.Int).Sub(header.BaseFee, remainder))
+		totalCost.Add(totalCost, new(big.Int).Sub(l2Batch.BaseFee, remainder))
 	}
 
-	return gasCost, nil
+	return totalCost, nil
 }
