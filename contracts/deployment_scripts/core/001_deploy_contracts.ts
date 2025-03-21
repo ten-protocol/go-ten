6dae3229aec8712329ff8d08b6c9f4b0850df7cc
Will Hester <william_hester@me.com>
2025-03-21 17:38:21 +0000

    comment out console logs cause it messes up docker

--- contracts/deployment_scripts/core/001_deploy_contracts.ts
+++ contracts/deployment_scripts/core/001_deploy_contracts.ts
@@ -73,20 +73,23 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
         log: true,
     });
 
-
-    console.log(`NetworkConfig= ${networkConfigDeployment.address}`);
+    console.log(`NetworkConfig= ${networkConfigDeployment.address}`); // line[0] in docker container
     console.log(`CrossChain= ${crossChainDeployment.address}`);
     console.log(`MerkleMessageBus= ${merkleMessageBusAddress}`);
     console.log(`NetworkEnclaveRegistry= ${networkEnclaveRegistryDeployment.address}`);
     console.log(`DataAvailabilityRegistry= ${daRegistryDeployment.address}`);
     console.log(`L1Start= ${networkConfigDeployment.receipt!!.blockHash}`);
 
-    // Get the MerkleTreeMessageBus contract instance
     const merkleMessageBusContract = await hre.ethers.getContractAt('MerkleTreeMessageBus', merkleMessageBusAddress);
-    console.log(`Adding DataAvailabilityRegistry (${daRegistryDeployment.address}) as state root manager...`);
+    // FIXME WARNING uncommenting these will mess up the address parsing in the docker container
+    // console.log(`Adding DataAvailabilityRegistry (${daRegistryDeployment.address}) as state root manager...`);
     const tx = await merkleMessageBusContract.addStateRootManager(daRegistryDeployment.address);
-    await tx.wait();
-    console.log('Successfully added DataAvailabilityRegistry as state root manager');
+    const receipt = await tx.wait();
+    if (receipt.status === 1) {
+        // console.log('Successfully added DataAvailabilityRegistry as state root manager');
+    } else {
+        // console.log(`Failed to add DataAvailabilityRegistry as state root manager`);
+    }
 };
 
 export default func;
