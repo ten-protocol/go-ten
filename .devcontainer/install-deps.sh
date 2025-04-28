025d30a68503d47e2188846578f1752210e47ca3
Tudor Malene <tudor.malene@gmail.com>
2025-04-28 15:43:54 +0300

    Upgrade go

--- .devcontainer/install-deps.sh
+++ .devcontainer/install-deps.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Install Go 1.23.5
-echo "Installing Go 1.23.5..."
-wget https://go.dev/dl/go1.23.5.linux-amd64.tar.gz
+# Install Go 1.23.8
+echo "Installing Go 1.23.8..."
+wget https://go.dev/dl/go1.23.8.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go
-sudo tar -C /usr/local -xzf go1.23.5.linux-amd64.tar.gz
-rm go1.23.5.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.23.8.linux-amd64.tar.gz
+rm go1.23.8.linux-amd64.tar.gz
 
 # Ensure Go is in the PATH
 export PATH=$PATH:/usr/local/go/bin
