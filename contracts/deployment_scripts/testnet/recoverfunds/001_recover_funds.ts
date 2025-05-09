e4efb47f0281f4137fffe0ba21d6fd16cd69c1a6
Will Hester <william_hester@me.com>
2025-05-09 12:04:35 +0100

    add 1.2 code to be uncommented after release

--- contracts/deployment_scripts/testnet/recoverfunds/001_recover_funds.ts
+++ contracts/deployment_scripts/testnet/recoverfunds/001_recover_funds.ts
@@ -17,11 +17,18 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const response = await axios.post(rpcUrl, rpcPayload, {
         headers: { 'Content-Type': 'application/json' }
     });
-    const bridgeContractAddress = response.data.result.L1MessageBus;
-    const bridgeContract = (await hre.ethers.getContractFactory('MessageBus')).attach(bridgeContractAddress) as MessageBus;
-    const tx = await bridgeContract.retrieveAllFunds("0x72788F3596dfdd656D64e337F6fdD72A0DEf9775");
-    const receipt = await tx.wait();
 
+    // For 1.1 use this. Hardcoded to gnosis safe:
+    const messageBusAddress = response.data.result.L1MessageBus;
+    const messageBusContract = (await hre.ethers.getContractFactory('MessageBus')).attach(messageBusAddress) as MessageBus;
+    const tx = await messageBusContract.retrieveAllFunds("0xeA052c9635F1647A8a199c2315B9A66ce7d1e2a7");
+
+    // After 1.2 has been released:
+    // const bridgeContractAddress = response.data.result.L1Bridge;
+    // const bridgeContract = (await hre.ethers.getContractFactory('TenBridge')).attach(bridgeContractAddress) as TenBridge;
+    // const tx = await bridgeContract.retrieveAllFunds();
+
+    const receipt = await tx.wait();
     if (receipt && receipt.status === 1) {
         console.log("Successfully recovered funds from the bridge.");
     } else {
