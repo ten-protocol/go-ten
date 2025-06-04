bdf2ed71d1d4b8862a736cd6a3acaed44ccc372f
Matt Curtis <matt@obscu.ro>
2025-06-04 12:29:02 +0100

    Fix capitalization

--- go/enclave/storage/enclavedb/config.go
+++ go/enclave/storage/enclavedb/config.go
@@ -20,7 +20,7 @@ const (
 const (
 	attInsert           = "insert into attestation (enclave_id, pub_key, node_type)  values (?,?,?)"
 	attSelect           = "select pub_key, node_type from attestation where enclave_id=?"
-	attExists           = "select EXISTS(select 1 from attestation where enclave_id = ?)"
+	attExists           = "select exists(select 1 from attestation where enclave_id = ?)"
 	attUpdateKey        = "update attestation set pub_key = ? where enclave_id = ?"
 	attUpdate           = "update attestation set node_type=? where enclave_id=?"
 	attSelectSequencers = "select enclave_id from attestation where node_type = ?"
