2904d8abc3cdef2244bd2554db41f39ffe3dd24b
Matt Curtis <matt@obscu.ro>
2025-06-05 12:28:45 +0100

    L1 deploy: bump 5min timeout to 10min

--- testnet/launcher/l1contractdeployer/docker.go
+++ testnet/launcher/l1contractdeployer/docker.go
@@ -75,7 +75,7 @@ func (n *ContractDeployer) RetrieveL1ContractAddresses() (*node.NetworkConfig, e
 	defer cli.Close()
 
 	// make sure the container has finished execution (5 minutes allows time for L1 transactions to be mined)
-	err = docker.WaitForContainerToFinish(n.containerID, 5*time.Minute)
+	err = docker.WaitForContainerToFinish(n.containerID, 10*time.Minute)
 	if err != nil {
 		return nil, err
 	}
