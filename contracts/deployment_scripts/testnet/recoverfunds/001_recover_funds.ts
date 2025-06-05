0003d87d67df80dfd6acac56fdd84a51fd5040be
badgersrus <43809877+badgersrus@users.noreply.github.com>
2025-06-05 13:55:40 +0100

    Enable funds recovery for new bridge (#2483)

--- contracts/deployment_scripts/testnet/recoverfunds/001_recover_funds.ts
+++ contracts/deployment_scripts/testnet/recoverfunds/001_recover_funds.ts
@@ -18,15 +18,10 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
         headers: { 'Content-Type': 'application/json' }
     });
 
-    // For 1.1 use this. Hardcoded to gnosis safe:
-    const messageBusAddress = response.data.result.L1MessageBus;
-    const messageBusContract = (await hre.ethers.getContractFactory('MessageBus')).attach(messageBusAddress) as MessageBus;
-    const tx = await messageBusContract.retrieveAllFunds("0xeA052c9635F1647A8a199c2315B9A66ce7d1e2a7");
-
-    // After 1.2 has been released:
-    // const bridgeContractAddress = response.data.result.L1Bridge;
-    // const bridgeContract = (await hre.ethers.getContractFactory('TenBridge')).attach(bridgeContractAddress) as TenBridge;
-    // const tx = await bridgeContract.retrieveAllFunds();
+    // recover funds from the new bridge contract
+    const bridgeContractAddress = response.data.result.L1Bridge;
+    const bridgeContract = (await hre.ethers.getContractFactory('TenBridge')).attach(bridgeContractAddress) as TenBridge;
+    const tx = await bridgeContract.retrieveAllFunds();
 
     const receipt = await tx.wait();
     if (receipt && receipt.status === 1) {
