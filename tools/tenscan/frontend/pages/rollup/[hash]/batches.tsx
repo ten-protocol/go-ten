6cb7dd6e4cc14667dee1946d58802ce3232df150
Will Hester <william_hester@me.com>
2025-06-06 10:19:37 +0100

    fix fe component

--- tools/tenscan/frontend/pages/rollup/[hash]/batches.tsx
+++ tools/tenscan/frontend/pages/rollup/[hash]/batches.tsx
@@ -39,6 +39,7 @@ export default function RollupBatches() {
             refetch={refetchRollupBatches}
             total={+Total}
             isLoading={isRollupBatchesLoading}
+            noResultsText="batches"
           />
         ) : (
           <p>Loading...</p>
