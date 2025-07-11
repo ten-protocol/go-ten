7083c065fdb51ceb2a354b4b80b5c04cbaaa230a
Žiga Kokelj <ziga.kokelj@gmail.com>
2025-07-11 11:56:42 +0200

    Revert "remove PlusCircledIcon"
    This reverts commit 32bea910c10ca6f1bedf7a3910dbd56a9eb697cc.

--- packages/ui/components/common/data-table/data-table-faceted-filter.tsx
+++ packages/ui/components/common/data-table/data-table-faceted-filter.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { CheckIcon } from "../../shared/react-icons";
+import { CheckIcon, PlusCircledIcon } from "../../shared/react-icons";
 import { Column } from "@tanstack/react-table";
 
 import { cn } from "../../../lib/utils";
@@ -39,7 +39,7 @@ export function DataTableFacetedFilter<TData, TValue>({
     <Popover>
       <PopoverTrigger asChild>
         <Button variant="outline" size="sm" className="h-8 border-dashed">
-          {/* <PlusCircledIcon className="mr-2 h-4 w-4" /> */}
+          <PlusCircledIcon className="mr-2 h-4 w-4" />
           {title}
           {selectedValues?.size > 0 && (
             <>
