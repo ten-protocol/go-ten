78996e16909f26c128278ebd165d837eabafd8ec
Matt Curtis <matt@obscu.ro>
2025-07-15 13:36:34 +0100

    separate the recover function into this testnet version of the contract

--- contracts/src/reference_bridge/L1/contracts/TenBridge.sol
+++ contracts/src/reference_bridge/L1/contracts/TenBridge.sol
@@ -159,20 +159,4 @@ contract TenBridge is
         require(sent, "Failed to send Ether");
         emit Withdrawal(receiver, address(0), amount);
     }
-
-    // This function is used to recover all native funds from the bridge contract - this is only available on testnets
-    function recoverTestnetFunds(address receiver) external onlyRole(ADMIN_ROLE) {
-        require(receiver != address(0), "Receiver cannot be 0x0");
-        require( // this check ensures this could not be used on mainnet if it were deployed there
-            block.chainid == 11155111 || block.chainid == 1337,
-            "Recovery only allowed on approved testnets"
-        );
-
-        uint256 balance = address(this).balance;
-        if (balance > 0) {
-            (bool sent, ) = receiver.call{value: balance}("");
-            require(sent, "Failed to recover Ether");
-            emit Withdrawal(receiver, address(0), balance);
-        }
-    }
 }
