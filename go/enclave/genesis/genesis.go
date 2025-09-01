db3aa3e41f5059914c7684930c4d1dd68a95907a
Tudor Malene <tudor.malene@gmail.com>
2025-09-01 14:11:52 +0100

    fix

--- go/enclave/genesis/genesis.go
+++ go/enclave/genesis/genesis.go
@@ -49,9 +49,13 @@ func (g Genesis) CommitGenesisState(storage storage.Storage) error {
 	if err != nil {
 		return err
 	}
-	if err := storage.TrieDB().Commit(root, true); err != nil {
-		return err
+
+	if root != (gethcommon.Hash{}) {
+		if err := storage.TrieDB().Commit(root, true); err != nil {
+			return err
+		}
 	}
+
 	return nil
 }
 
