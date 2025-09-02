e05bc8968cf2bdc3ef6f4a98b407a3e083d65e27
Tudor Malene <tudor.malene@gmail.com>
2025-09-02 08:24:36 +0100

    xchain bug

--- go/enclave/components/batch_executor.go
+++ go/enclave/components/batch_executor.go
@@ -603,7 +603,7 @@ func (executor *batchExecutor) postProcessState(ec *BatchExecutionContext) error
 
 	for _, msg := range valueTransferMessages {
 		bal := ec.stateDB.GetBalance(executor.config.BridgeAddress).Uint64()
-		if bal > msg.Amount.Uint64() {
+		if bal >= msg.Amount.Uint64() {
 			ec.stateDB.SubBalance(executor.config.BridgeAddress, uint256.MustFromBig(msg.Amount), tracing.BalanceChangeUnspecified)
 		} else {
 			executor.logger.Error("Insufficient balance for value transfer", "balance", bal, "amount", msg.Amount)
