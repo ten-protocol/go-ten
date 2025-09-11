fc758183213e5e2a6039fb1801aaf28f3f32816b
Žiga Kokelj <ziga.kokelj@gmail.com>
2025-09-11 13:21:09 +0200

    use unique chain IDs for each testnet (#2652)

--- packages/ui/lib/enums/network.ts
+++ packages/ui/lib/enums/network.ts
@@ -7,10 +7,11 @@ export enum L1Network {
 }
 
 export enum L2Network {
-  SEPOLIA = 443,
-  UAT = 443,
-  DEV = 443,
-  LOCAL = 443,
+  MAINNET = 443,
+  SEPOLIA = 8443,
+  UAT = 7443,
+  DEV = 6443,
+  LOCAL = 5443,
 }
 
 export enum TransactionStatus {
