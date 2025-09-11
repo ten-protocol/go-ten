426b3d398160cf41dd8ce33c5703cc46fd3d2faa
Tudor Malene <tudor.malene@gmail.com>
2025-09-11 09:06:55 +0100

    split kv table (and rename)

--- go/enclave/storage/init/edgelessdb/002_init.sql
+++
@@ -1,2 +0,0 @@
-drop index receipt on tendb.receipt_viewer;
-CREATE INDEX receipt ON tendb.receipt_viewer (eoa, receipt);
--- go/enclave/storage/init/sqlite/002_init.sql
+++
@@ -1,2 +0,0 @@
-DROP INDEX IDX_REC_VIEW;
-CREATE INDEX IDX_REC_VIEW ON receipt_viewer (eoa, receipt);
