99357447329dd639114479276a266d26d37a9635
Tudor Malene <tudor.malene@gmail.com>
2025-09-16 18:08:02 +0200

    add unique index on key

---
+++ go/enclave/storage/init/edgelessdb/002_unique_ky.sql
@@ -0,0 +1,6 @@
+SET SESSION  rocksdb_max_row_locks = 10000000;
+DELETE s1 FROM statedb32 s1 INNER JOIN statedb32 s2 ON s1.ky = s2.ky AND s1.id > s2.id;
+ALTER TABLE statedb32 ADD CONSTRAINT unique_ky_statedb32 UNIQUE (ky);
+
+DELETE FROM statedb64  WHERE id NOT IN (SELECT MIN(id) FROM statedb64 GROUP BY ky);
+ALTER TABLE statedb64 ADD CONSTRAINT unique_ky_statedb64 UNIQUE (ky);
