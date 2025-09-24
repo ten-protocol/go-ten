ea92bd59928c326178a1ec9b586facb79954a3c3
Will Hester <william_hester@me.com>
2025-09-24 16:35:19 +0100

    remove fe code from go-ten

--- tools/bridge-frontend/src/schemas/bridge.ts
+++
@@ -1,9 +0,0 @@
-import * as yup from "yup";
-
-export const bridgeSchema = yup.object().shape({
-  amount: yup.string().required("Amount is required"),
-  fromChain: yup.string().required("From Chain is required"),
-  toChain: yup.string().required("To Chain is required"),
-  token: yup.string().required("Select a token"),
-  receiver: yup.string().required("Receiver is required"),
-});
--- tools/walletextension/frontend/src/types/interfaces/WalletInterfaces.ts
+++
@@ -1,4 +0,0 @@
-export type Account = {
-    name: string;
-    connected: boolean;
-};
