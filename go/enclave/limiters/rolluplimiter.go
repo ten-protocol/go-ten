5e97edca1f71659fc32c8158c1d4557b5482d094
Will Hester <william_hester@me.com>
2025-09-30 14:25:20 +0100

    reduce to 2

--- go/enclave/limiters/rolluplimiter.go
+++ go/enclave/limiters/rolluplimiter.go
@@ -12,7 +12,11 @@ const (
 	// 85% is a very conservative number. It will most likely be 66% in practice.
 	// We can lower it, once we have a mechanism in place to handle batches that don't actually compress to that.
 	txCompressionFactor  = 0.85
-	compressedHeaderSize = 1
+	// Based on testing: compressedHeaderSize=4 gives ~17K batches (too conservative)
+	// Target: ~25K batches for better utilization of 90KB limit
+	// Current: 2.06 bytes/batch actual, target estimation: 3.6 bytes/batch
+	// With 0.85 factor: 2.55 + 1.5 = 4.05 bytes/batch ≈ 22,222 batches
+	compressedHeaderSize = 2
 )
 
 type rollupLimiter struct {
