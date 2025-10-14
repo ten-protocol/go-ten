f8246d838241e20c0225b67408dd1f38f60fa80e
Matt Curtis <matt@obscu.ro>
2025-10-14 12:20:10 +0100

    enclave: always cache MA fees on calculation

--- go/enclave/gas/oracle.go
+++ go/enclave/gas/oracle.go
@@ -2,12 +2,13 @@ package gas
 
 import (
 	"context"
+	"encoding/base64"
 	"fmt"
 	"math/big"
-	"sync"
 
 	gethlog "github.com/ethereum/go-ethereum/log"
 
+	"github.com/TwiN/gocache/v2"
 	"github.com/ethereum/go-ethereum/consensus/misc/eip4844"
 	"github.com/ten-protocol/go-ten/go/enclave/storage"
 
@@ -24,19 +25,20 @@ import (
 // - note that increasing this value will have to increase the number of cached blocks in the cache_service
 const MovingAverageWindow = 300 // `3600 / 12` - last 1 hour
 
-// MaxHistoricMA - the maximum number of historic blocks
-const MaxHistoricMA = 50
-
 // L1TxGas - a crude estimation of the cost of publishing an L1 tx
 const L1TxGas = 300_000
 
 // TxsPerRollup - the number of transactions in a rollup. A conservative estimation.
 const TxsPerRollup = 250
 
+type CachedFeeMA struct {
+	BaseFee *big.Int
+	BlobFee *big.Int
+}
+
 // Oracle - the interface for the future precompiled gas oracle contract
 // which will expose necessary l1 information.
 type Oracle interface {
-	SubmitL1Block(ctx context.Context, headBlock *types.Header) error
 	EstimateL1StorageGasCost(ctx context.Context, tx *types.Transaction, block *types.Header, header *common.BatchHeader) (*big.Int, error)
 	EstimateL1CostForMsg(ctx context.Context, args *gethapi.TransactionArgs, header *common.BatchHeader) (*big.Int, error)
 }
@@ -44,22 +46,26 @@ type Oracle interface {
 type oracle struct {
 	l1ChainCfg *params.ChainConfig
 	storage    storage.BlockResolver
-	headMutex  sync.RWMutex
-	headBlock  *types.Header
-	// hold the moving average of the base fee and blob fee per block
-	blobFeeMA map[uint64]*big.Int
-	baseFeeMA map[uint64]*big.Int
-	logger    gethlog.Logger
+	feeMACache *gocache.Cache
+	logger     gethlog.Logger
 }
 
 func NewGasOracle(l1ChainCfg *params.ChainConfig, storage storage.BlockResolver, logger gethlog.Logger) Oracle {
+	cache := gocache.NewCache().
+		WithMaxSize(50).
+		WithEvictionPolicy(gocache.FirstInFirstOut).
+		WithDefaultTTL(gocache.NoExpiration)
+
+	err := cache.StartJanitor()
+	if err != nil {
+		logger.Crit("Could not initialise fee MA cache", "error", err)
+	}
+
 	return &oracle{
 		l1ChainCfg: l1ChainCfg,
 		storage:    storage,
 		logger:     logger,
-		headMutex:  sync.RWMutex{},
-		baseFeeMA:  make(map[uint64]*big.Int),
-		blobFeeMA:  make(map[uint64]*big.Int),
+		feeMACache: cache,
 	}
 }
 
@@ -80,28 +86,6 @@ func (o *oracle) EstimateL1CostForMsg(ctx context.Context, args *gethapi.Transac
 	}
 
 	return o.calculateL1Cost(ctx, nil, header, encoded)
-}
-
-func (o *oracle) SubmitL1Block(ctx context.Context, headBlock *types.Header) error {
-	o.headMutex.Lock()
-	defer o.headMutex.Unlock()
-	o.headBlock = headBlock
-	blockNum := headBlock.Number.Uint64()
-
-	baseFeeMA, blobFeeMA, err := o.calculateMA(ctx, blockNum)
-	if err != nil {
-		return err
-	}
-
-	o.baseFeeMA[blockNum] = baseFeeMA
-	o.blobFeeMA[blockNum] = blobFeeMA
-
-	if blockNum > MaxHistoricMA {
-		// cleanup entries older than MaxHistoricMA
-		delete(o.baseFeeMA, blockNum-MaxHistoricMA)
-		delete(o.blobFeeMA, blockNum-MaxHistoricMA)
-	}
-	return nil
 }
 
 // calculateMA - calculates the baseFee and blobFee MA for the specified block
@@ -142,30 +126,27 @@ func (o *oracle) calculateMA(ctx context.Context, blockHeight uint64) (*big.Int,
 // calculateL1Cost - Calculates the L1 cost as a multiple of the L2 base fee.
 // it takes into account the share of the blob cost and the share of the L1 TX cost - which submits and stores the rollup header.
 func (o *oracle) calculateL1Cost(ctx context.Context, block *types.Header, l2Batch *common.BatchHeader, encodedTx []byte) (*big.Int, error) {
-	o.headMutex.RLock()
-	defer o.headMutex.RUnlock()
-
 	if block == nil {
-		block = o.headBlock
+		var err error
+		block, err = o.storage.FetchHeadBlock(ctx)
+		if err != nil {
+			return nil, fmt.Errorf("no L1 block available for gas calculation and failed to fetch head block: %w", err)
+		}
 	}
 
 	shareOfBlobCost := big.NewInt(0)
 	txL1Size := CalculateL1Size(encodedTx)
 
-	var err error
-	baseFee := o.baseFeeMA[block.Number.Uint64()]
-	blobFee, f := o.blobFeeMA[block.Number.Uint64()]
-	// the block MA is not in the cache, so we're calculating
-	if !f {
-		o.logger.Info("L1 Gas MA not found in cache, calculating it", "block_number", block.Number.Uint64())
-		baseFee, blobFee, err = o.calculateMA(ctx, block.Number.Uint64())
-		if err != nil {
-			return nil, err
-		}
+	cacheKey := block.Hash().Bytes()
+	cachedMA, err := o.getCachedFeeMA(ctx, cacheKey, block.Number.Uint64())
+	if err != nil {
+		return nil, err
 	}
 
+	baseFee := cachedMA.BaseFee
+	blobFee := cachedMA.BlobFee
+
 	// 1. Calculate the cost of including the tx in a blob
-	// price in Wei for a single unit of blob
 	if isNonZero(blobFee) {
 		shareOfBlobCost = big.NewInt(0).Mul(txL1Size, blobFee)
 	}
@@ -189,6 +170,36 @@ func (o *oracle) calculateL1Cost(ctx context.Context, block *types.Header, l2Bat
 	}
 
 	return totalCost, nil
+}
+
+func (o *oracle) getCachedFeeMA(ctx context.Context, cacheKey []byte, blockHeight uint64) (*CachedFeeMA, error) {
+	keyStr := base64.StdEncoding.EncodeToString(cacheKey)
+
+	value, found := o.feeMACache.Get(keyStr)
+	if found {
+		feeMA, ok := value.(*CachedFeeMA)
+		if !ok {
+			// this should never happen
+			return nil, fmt.Errorf("invalid type in fee MA cache")
+		}
+		return feeMA, nil
+	}
+
+	o.logger.Info("L1 Gas MA not found in cache, calculating it", "block_height", blockHeight)
+
+	baseFee, blobFee, err := o.calculateMA(ctx, blockHeight)
+	if err != nil {
+		return nil, err
+	}
+
+	cachedMA := &CachedFeeMA{
+		BaseFee: baseFee,
+		BlobFee: blobFee,
+	}
+
+	o.feeMACache.Set(keyStr, cachedMA)
+
+	return cachedMA, nil
 }
 
 func isNonZero(nr *big.Int) bool {
