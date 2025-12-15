c7b369818b71421ee4bc45a5c06a0492e81f7500
badgersrus <43809877+badgersrus@users.noreply.github.com>
2025-12-15 12:26:03 +0000

    Disable rollups & bridge upgrade/ workflows on releases/v1.6 (#2782)

--- testnet/launcher/fundsrecovery/docker.go
+++ testnet/launcher/fundsrecovery/docker.go
@@ -5,12 +5,13 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/docker/docker/api/types/container"
 
 	"github.com/docker/docker/client"
-	"github.com/sanity-io/litter"
 	"github.com/ten-protocol/go-ten/go/common/docker"
 )
 
@@ -26,33 +27,46 @@ func NewFundsRecovery(cfg *Config) (*FundsRecovery, error) {
 }
 
 func (n *FundsRecovery) Start() error {
-	fmt.Printf("Starting L2 contract deployer with config: \n%s\n\n", litter.Sdump(*n.cfg))
-
+	fmt.Printf("Starting funds recovery with config: %s\n", n.cfg)
+	var err error
 	cmds := []string{
-		"npx", "hardhat", "deploy",
-		"--network", "layer1",
+		"npx",
+		"hardhat",
+		"run",
+		"--network",
+		"layer1",
+		"scripts/recover/recover_testnet_funds.ts",
+		"--verbose",
 	}
 
 	envs := map[string]string{
 		"NETWORK_JSON": fmt.Sprintf(`
-{
-        "layer1" : {
-            "url" : "%s",
-            "useGateway" : false,
-            "live" : false,
-            "saveDeployments" : true,
-            "deploy": [
-                "deployment_scripts/testnet/recoverfunds"
-            ],
-            "accounts": [
-                "%s"
-            ]
-        }
-    }
-`, n.cfg.l1HTTPURL, n.cfg.l1privateKey),
+		{
+			"layer1": {
+				"url": "%s",
+				"useGateway": false,
+				"live": false,
+				"saveDeployments": true,
+				"accounts": ["%s"]
+			}
+		}`, n.cfg.l1HTTPURL, n.cfg.l1privateKey),
+		"NETWORK_CONFIG_ADDR": n.cfg.networkConfigAddress,
+		"RECEIVER_ADDRESS":    n.cfg.receiverAddress,
 	}
 
-	containerID, err := docker.StartNewContainer("recover-funds", n.cfg.dockerImage, cmds, nil, envs, nil, nil, false)
+	// Mount only the scripts directory to use updated scripts
+	// This avoids conflicts with node_modules which are platform-specific
+	cwd, err := os.Getwd()
+	if err != nil {
+		return fmt.Errorf("failed to get current working directory: %w", err)
+	}
+	scriptsPath := filepath.Join(cwd, "contracts", "scripts")
+	fmt.Printf("Mounting local scripts directory: %s\n", scriptsPath)
+	volumes := map[string]string{
+		scriptsPath: "/home/obscuro/go-obscuro/contracts/scripts",
+	}
+
+	containerID, err := docker.StartNewContainer("recover-funds", n.cfg.dockerImage, cmds, nil, envs, nil, volumes, false)
 	if err != nil {
 		return err
 	}
