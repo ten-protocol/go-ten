ce403ad7f257eb2470007405f9d31c77d85e27b5
Will Hester <william_hester@me.com>
2025-12-19 13:08:12 +0000

    clarify desc

--- go/config/network.go
+++ go/config/network.go
@@ -57,7 +57,7 @@ type L1Config struct {
 	BlockTime        time.Duration   `mapstructure:"blockTime"`        // average expected block time for the L1 network
 	RollupRetryDelay time.Duration   `mapstructure:"rollupRetryDelay"` // delay rollup publishing when gas price spikes
 	StartHash        gethcommon.Hash `mapstructure:"startHash"`        // hash of the first block on the L1 network relevant to the Ten network
-	TimeoutBlocks    int             `mapstructure:"timeoutBlocks"`    // number of blocks we wait before disconnecting if no blocks seen
+	TimeoutBlocks    int             `mapstructure:"timeoutBlocks"`    // number of expected block intervals (expectedBlockTime) to wait with no new blocks before timing out
 	MaxBlobRetries   int             `mapstructure:"maxBlobRetries"`   // number of retry attempts to publish a blob
 	L1Contracts      *L1Contracts    `mapstructure:"contracts"`
 }
