832e55a32f544879d7d54291f8a2e05c13147d6b
Žiga Kokelj <ziga.kokelj@gmail.com>
2026-01-29 16:27:00 +0100

    gw attestation -use intel PCS for verification collateral (#2819)

--- tools/walletextension/main/entry.sh
+++ tools/walletextension/main/entry.sh
@@ -11,11 +11,14 @@ if [ ! -L /dev/sgx/enclave ]; then
 	ln -s /dev/sgx_enclave /dev/sgx/enclave
 fi
 
-PCCS_URL=https://global.acccache.azure.net/sgx/certification/v4/
+PCCS_URL=${PCCS_URL:-https://global.acccache.azure.net/sgx/certification/v4/}
+COLLATERAL_SERVICE=${COLLATERAL_SERVICE:-https://api.trustedservices.intel.com/sgx/certification/v4/}
+
 echo "PCCS_URL: ${PCCS_URL}"
+echo "COLLATERAL_SERVICE: ${COLLATERAL_SERVICE}"
 
 apt-get install -qq libsgx-dcap-default-qpl
 
-echo "PCCS_URL=${PCCS_URL}\nUSE_SECURE_CERT=FALSE" > /etc/sgx_default_qcnl.conf
+echo "PCCS_URL=${PCCS_URL}\nCOLLATERAL_SERVICE=${COLLATERAL_SERVICE}\nUSE_SECURE_CERT=FALSE" > /etc/sgx_default_qcnl.conf
 
 "$@"
